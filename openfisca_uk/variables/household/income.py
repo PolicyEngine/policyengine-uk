feb8ef5a216472ac2967b6ec6a8b38fbf158e4b1
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2020-10-06 20:30:13 +0100

    Fix service charges and AHC equivalisation

--- openfisca_uk/variables/household/income.py
+++ openfisca_uk/variables/household/income.py
@@ -3,13 +3,6 @@ from openfisca_uk.entities import *
 import numpy as np
 
 
-class HHINC(Variable):
-    value_type = float
-    entity = Household
-    label = u"Gross household income FRS variable"
-    definition_period = ETERNITY
-
-
 class council_tax(Variable):
     value_type = float
     entity = Household
@@ -17,12 +10,17 @@ class council_tax(Variable):
     definition_period = ETERNITY
 
 
-class housing_cost(Variable):
+class housing_costs(Variable):
     value_type = float
     entity = Household
     label = u"Housing costs per week"
     definition_period = ETERNITY
 
+class service_charges(Variable):
+    value_type = float
+    entity = Household
+    label = u'Amount paid for Service Charges/Ground Rent'
+    definition_period = ETERNITY
 
 class household_gross_income(Variable):
     value_type = float
@@ -43,7 +41,7 @@ class household_net_income_bhc(Variable):
     def formula(household, period, parameters):
         return household.sum(
             household.members("net_income", period)
-        ) - household("council_tax", period)
+        ) - household("council_tax", period) - household("service_charges", period)
 
 
 class equiv_household_net_income_bhc(Variable):
@@ -54,7 +52,7 @@ class equiv_household_net_income_bhc(Variable):
 
     def formula(household, period, parameters):
         return household("household_net_income_bhc", period) / household(
-            "household_equivalisation", period
+            "household_equivalisation_bhc", period
         )
 
 
@@ -68,7 +66,8 @@ class household_net_income_ahc(Variable):
         return (
             household.sum(household.members("net_income", period))
             - household("council_tax", period)
-            - household("housing_cost", period)
+            - household("housing_costs", period)
+            - household("service_charges", period)
         )
 
 
@@ -80,5 +79,5 @@ class equiv_household_net_income_ahc(Variable):
 
     def formula(household, period, parameters):
         return household("household_net_income_ahc", period) / household(
-            "household_equivalisation", period
+            "household_equivalisation_ahc", period
         )
