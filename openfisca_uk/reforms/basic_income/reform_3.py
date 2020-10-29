d7f07595ae3c85c424e4bd80cf716e2a47d6232a
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2020-10-29 22:32:55 +0000

    Update reforms

--- openfisca_uk/reforms/basic_income/reform_3.py
+++ openfisca_uk/reforms/basic_income/reform_3.py
@@ -2,6 +2,24 @@ from openfisca_core.model_api import *
 from openfisca_uk.entities import *
 
 
+class income_tax_applicable_amount(Variable):
+    value_type = float
+    entity = Person
+    label = u"Total taxable income per week"
+    definition_period = ETERNITY
+
+    def formula(person, period, parameters):
+        return max_(
+            person("employee_earnings", period)
+            + person("self_employed_earnings", period)
+            + person("state_pension", period)
+            + person("pension_income", period)
+            + person("taxed_means_tested_bonus", period)
+            + person("interest", period),
+            0,
+        )
+
+
 class income_tax(Variable):
     value_type = float
     entity = Person
@@ -10,7 +28,6 @@ class income_tax(Variable):
 
     def formula(person, period, parameters):
         return 0.5 * person("income_tax_applicable_amount", period)
-
 
 class NI(Variable):
     value_type = float
@@ -30,7 +47,13 @@ class basic_income(Variable):
     definition_period = ETERNITY
 
     def formula(person, period, parameters):
-        return 110 + person("is_state_pension_age", period) * 60
+        seniors = person("is_senior", period)
+        WA_adults = person("is_working_age_adult", period)
+        children = person("is_child", period)
+        is_disabled = person("disabled", period)
+        disabled_adults = (seniors * WA_adults) * is_disabled
+        disabled_children = children * is_disabled
+        return 175 * seniors + 116 * WA_adults + 58 * children + 58 * disabled_adults + 116 * disabled_children
 
 
 class benunit_basic_income(Variable):
@@ -41,6 +64,15 @@ class benunit_basic_income(Variable):
 
     def formula(benunit, period, parameters):
         return benunit.sum(benunit.members("basic_income", period))
+
+class household_basic_income(Variable):
+    value_type = float
+    entity = Household
+    label = u'Amount of basic income per week for the benefit unit'
+    definition_period = ETERNITY
+
+    def formula(household, period, parameters):
+        return household.sum(household.members("basic_income", period))
 
 
 class non_means_tested_bonus(Variable):
@@ -57,7 +89,7 @@ class reform_3(Reform):
     def apply(self):
         for changed_var in [income_tax, NI, non_means_tested_bonus]:
             self.update_variable(changed_var)
-        for added_var in [basic_income, benunit_basic_income]:
+        for added_var in [basic_income, benunit_basic_income, household_basic_income]:
             self.add_variable(added_var)
         for removed_var in [
             "child_benefit",
--- openfisca_uk/reforms/basic_income/reform_4.py
+++ openfisca_uk/reforms/basic_income/reform_4.py
@@ -2,6 +2,24 @@ from openfisca_core.model_api import *
 from openfisca_uk.entities import *
 
 
+class income_tax_applicable_amount(Variable):
+    value_type = float
+    entity = Person
+    label = u"Total taxable income per week"
+    definition_period = ETERNITY
+
+    def formula(person, period, parameters):
+        return max_(
+            person("employee_earnings", period)
+            + person("self_employed_earnings", period)
+            + person("state_pension", period)
+            + person("pension_income", period)
+            + person("taxed_means_tested_bonus", period)
+            + person("interest", period),
+            0,
+        )
+
+
 class income_tax(Variable):
     value_type = float
     entity = Person
@@ -10,7 +28,6 @@ class income_tax(Variable):
 
     def formula(person, period, parameters):
         return 0.5 * person("income_tax_applicable_amount", period)
-
 
 class NI(Variable):
     value_type = float
@@ -30,7 +47,13 @@ class basic_income(Variable):
     definition_period = ETERNITY
 
     def formula(person, period, parameters):
-        return person("is_adult", period) * 115
+        seniors = person("is_senior", period)
+        WA_adults = person("is_working_age_adult", period)
+        children = person("is_child", period)
+        is_disabled = person("disabled", period)
+        disabled_adults = (seniors * WA_adults) * is_disabled
+        disabled_children = children * is_disabled
+        return 175 * seniors + 107 * (WA_adults + children) + 107 * (disabled_adults + disabled_children)
 
 
 class benunit_basic_income(Variable):
@@ -42,6 +65,15 @@ class benunit_basic_income(Variable):
     def formula(benunit, period, parameters):
         return benunit.sum(benunit.members("basic_income", period))
 
+class household_basic_income(Variable):
+    value_type = float
+    entity = Household
+    label = u'Amount of basic income per week for the benefit unit'
+    definition_period = ETERNITY
+
+    def formula(household, period, parameters):
+        return household.sum(household.members("basic_income", period))
+
 
 class non_means_tested_bonus(Variable):
     value_type = float
@@ -50,27 +82,25 @@ class non_means_tested_bonus(Variable):
     definition_period = ETERNITY
 
     def formula(person, period, parameters):
-        return min_(25, person("basic_income", period))
-
-
-class untaxed_means_tested_bonus(Variable):
-    value_type = float
-    entity = Person
-    label = u"Amount of the basic income which is subject to means tests"
-    definition_period = ETERNITY
-
-    def formula(person, period, parameters):
-        return max_(0, person("basic_income", period) - 25)
+        return person("basic_income", period)
 
 
 class reform_4(Reform):
     def apply(self):
-        for changed_var in [
-            income_tax,
-            NI,
-            untaxed_means_tested_bonus,
-            non_means_tested_bonus,
-        ]:
+        for changed_var in [income_tax, NI, non_means_tested_bonus]:
             self.update_variable(changed_var)
-        for added_var in [basic_income, benunit_basic_income]:
+        for added_var in [basic_income, benunit_basic_income, household_basic_income]:
             self.add_variable(added_var)
+        for removed_var in [
+            "child_benefit",
+            "income_support",
+            "JSA_contrib",
+            "JSA_income",
+            "child_tax_credit",
+            "working_tax_credit",
+            "universal_credit",
+            "state_pension",
+            "housing_benefit",
+            "pension_credit",
+        ]:
+            self.neutralize_variable(removed_var)
