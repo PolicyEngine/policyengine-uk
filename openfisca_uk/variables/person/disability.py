bb3cbb447d84473f6f05fa4d238844e162c1926f
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2020-12-01 17:02:48 +0000

    Add new disability variable

--- openfisca_uk/variables/person/disability.py
+++ openfisca_uk/variables/person/disability.py
@@ -16,7 +16,7 @@ class is_disabled(Variable):
             "SDA",
             "AA",
             "DLA_M",
-            "DLA_SC",
+            "DLA_SC"
         ]
         return add(person, period, QUALIFYING_BENEFITS, options=[MATCH]) > 0
 
@@ -60,3 +60,38 @@ class is_carer(Variable):
             >= parameters(period).benefits.carers_allowance.min_hours
         )
         return already_claiming + meets_requirements
+
+class registered_disabled(Variable):
+    value_type = bool
+    entity = Person
+    label = u'Whether registered disabled'
+    definition_period = YEAR
+
+class dis_equality_act_core(Variable):
+    value_type = bool
+    entity = Person
+    label = u'Whether disabled under the Equality Act (core definition)'
+    definition_period = YEAR
+
+class dis_equality_act_wider(Variable):
+    value_type = bool
+    entity = Person
+    label = u'Whether disabled under the Equality Act (wider definition)'
+    definition_period = YEAR
+
+class is_standard_disabled(Variable):
+    value_type = bool
+    entity = Person
+    label = u'Whether meets a basic level of disability'
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        QUALIFYING = [
+            "is_disabled",
+            "is_severely_disabled",
+            "is_enhanced_disabled",
+            "IIDB",
+            "registered_disabled",
+            "dis_equality_act_core"
+        ]
+        return add(person, period, QUALIFYING, options=[MATCH]) + person.benunit("ESA_income", period, options=[MATCH]) > 0
