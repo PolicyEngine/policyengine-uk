bc39b2aa0a1d18cefe72e0e7cda21a2c3188189c
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2020-12-14 10:21:20 +0000

    Add poverty gap variables

--- openfisca_uk/variables/household/attributes.py
+++ openfisca_uk/variables/household/attributes.py
@@ -183,3 +183,26 @@ class in_poverty_ahc(Variable):
             )
             < parameters(period).poverty.absolute_poverty_threshold_ahc
         )
+
+class poverty_gap_bhc(Variable):
+    value_type = float
+    entity = Household
+    label = u'Positive financial gap between net household income and the poverty line'
+    definition_period = WEEK
+
+    def formula(household, period, parameters):
+        poverty_line = parameters(period).poverty.absolute_poverty_threshold_bhc / household("household_equivalisation_bhc", period.this_year)
+        net_income = household("household_net_income", period, options=[DIVIDE])
+        return max_(0, poverty_line - net_income)
+
+
+class poverty_gap_ahc(Variable):
+    value_type = float
+    entity = Household
+    label = u'Positive financial gap between net household income and the poverty line, after housing costs'
+    definition_period = WEEK
+
+    def formula(household, period, parameters):
+        poverty_line = parameters(period).poverty.absolute_poverty_threshold_ahc / household("household_equivalisation_ahc", period.this_year)
+        net_income = household("household_net_income_ahc", period, options=[DIVIDE])
+        return max_(0, poverty_line - net_income)
