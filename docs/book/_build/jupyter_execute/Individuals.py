cb547f22d4b839abd2bd5fda1190b0fa0086414b
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2020-12-14 21:44:49 +0000

    Add individuals demo

---
+++ docs/book/_build/jupyter_execute/Individuals.py
@@ -0,0 +1,27 @@
+# Individual Simulations
+
+Individual-focused simulations calculate variables for hypothetical, manually specified people, benefit units and households. This can be useful for different applications:
+- Calculating taxes and benefits for a specific group
+- Evaluating how the computed variables change when one property is varied
+
+## Example: calculating the effective marginal tax rate schedule
+
+In this example, we specify the input data being a lone parent with one child. By setting the ```earnings``` field to vary (by default, between £0 and £200k) we can plot the line graph of earnings against the effective marginal tax rate. 
+
+Note that ```universal_credit_reported``` is required here - the UK is currently phasing out legacy benefits and phasing in the new Universal Credit benefit - we decide which to simulate based on reported receipt. 
+
+from openfisca_uk import IndividualSim
+import plotly.express as px
+
+sim = IndividualSim()
+sim.add_person(name="parent", age=24, is_benunit_head=True)
+sim.add_person(name="child", age=2)
+sim.add_benunit(adults=["parent"], children=["child"], universal_credit_reported=True)
+sim.add_household(adults=["parent"], children=["child"])
+
+sim.vary("earnings")
+earnings = sim.calc("earnings", target="parent")
+mtr = sim.calc_mtr(target="parent")
+fig = px.line(x=earnings, y=mtr, title="Effective marginal tax schedule")
+fig.update_layout(yaxis_tickformat="%", xaxis_tickprefix="£", xaxis_title="Earnings", yaxis_title="Marginal tax rate")
+fig.show()
