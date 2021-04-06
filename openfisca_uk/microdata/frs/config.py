ec3490d5da4b12fbb6ad76aa5e0e1d7808ed84c7
Nikhil <nikhil.woodruff@outlook.com>
2021-04-07 00:27:29 +0100

    Add initial FRS variables

--- openfisca_uk/microdata/frs/config.py
+++ openfisca_uk/microdata/frs/config.py
@@ -2,43 +2,83 @@ from openfisca_core.model_api import *
 from openfisca_uk.entities import *
 from openfisca_uk.tools.general import *
 
-
-class P_INEARNS(Variable):
-    value_type = float
-    entity = Person
-    label = u"Gross earnings from employment"
-    definition_period = WEEK
+# FRS variables
 
 class P_GROSS4(Variable):
     value_type = float
     entity = Person
-    label = u"Weight for adults"
     definition_period = YEAR
 
 class B_GROSS4(Variable):
     value_type = float
     entity = BenUnit
-    label = u"Weight for benefit units (families)"
     definition_period = YEAR
 
 class H_GROSS4(Variable):
     value_type = float
     entity = Household
-    label = u"Weight for households"
     definition_period = YEAR
 
-class earnings(Variable):
+class P_UGRSPAY(Variable):
     value_type = float
     entity = Person
-    label = u"Gross earnings from employment"
+    definition_period = WEEK
+
+class P_INPENINC(Variable):
+    value_type = float
+    entity = Person
+    definition_period = WEEK
+
+class P_PROFIT1(Variable):
+    value_type = float
+    entity = Person
+    definition_period = WEEK
+
+# Input variables
+
+class employment_income(Variable):
+    value_type = float
+    entity = Person
+    label = u'Income from employment'
     definition_period = YEAR
+    reference = "Income Tax (Earnings and Pensions) Act 2003 s. 1(1)(a)"
 
     def formula(person, period, parameters):
-        return person("P_INEARNS", period, options=[ADD])
+        return person("P_UGRSPAY", period, options=[ADD])
 
+class pension_income(Variable):
+    value_type = float
+    entity = Person
+    label = u'Income from pensions'
+    definition_period = YEAR
+    reference = "Income Tax (Earnings and Pensions) Act 2003 s. 1(1)(b)"
 
-FRS_variables = [P_INEARNS, P_GROSS4, B_GROSS4, H_GROSS4]
-input_variables = [earnings]
+    def formula(person, period, parameters):
+        return person("P_INPENINC", period, options=[ADD])
+
+class trading_income(Variable):
+    value_type = float
+    entity = Person
+    label = u'Income from trading profits'
+    definition_period = YEAR
+    reference = "Income Tax (Trading and Other Income) Act 2005 s. 1(1)(a)"
+
+    def formula(person, period, parameters):
+        return person("P_PROFIT1", period, options=[ADD])
+
+FRS_variables = [
+    P_GROSS4, 
+    B_GROSS4, 
+    H_GROSS4,
+    P_UGRSPAY,
+    P_INPENINC,
+    P_PROFIT1,
+]
+input_variables = [
+    employment_income,
+    pension_income,
+    trading_income,
+]
 
 
 class from_FRS(Reform):
