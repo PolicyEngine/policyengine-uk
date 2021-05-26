ec546afd95776f96fd4cb730e6de8d7e3dda1fd5
Nikhil <nikhil.woodruff@outlook.com>
2021-05-26 15:45:01 +0100

    Update documentation

--- docs/book/_build/jupyter_execute/index.py
+++ docs/book/_build/jupyter_execute/index.py
@@ -1,58 +1,67 @@
-# OpenFisca-UK
+#!/usr/bin/env python
+# coding: utf-8
 
-This book contains an introduction to using OpenFisca-UK to model UK taxes and benefits. It is currently a work in progress and may be added to.
+# # OpenFisca-UK
+# 
+# This book contains an introduction to using OpenFisca-UK to model UK taxes and benefits. It is currently a work in progress and may be added to. OpenFisca-UK is a microsimulation model of the UK tax and benefit system: it is a model which calculates variable values over UK entities from given policy parameters and structures. In practice, this gives it two main uses: calculating statistics under current tax and benefit law, and simulating effects of potential new changes to the legislation.
+# 
+# ## Short demo
+# 
+# ### Baseline estimation
+# 
+# Calculating, for example, the total tax (Income Tax and National Insurance) liability by region can be done with the following code:
 
-## Contents
+# In[1]:
 
-This book contains two main pages:
-- Populations: demonstrating country-wide simulations using survey microdata.
-- Individuals: demonstrating calculations and projections for hypothetical manually-defined people and groups.
 
-## Short demo
-
-Calculating, for example, the average (employee-side) National Insurance liability by region can be done with the following code:
-
-from openfisca_uk import PopulationSim # shortcut to FRS-populated simulations
-from openfisca_uk.variables.household.attributes import Region # for decoding region IDs
+from openfisca_uk import Microsimulation
 import pandas as pd
-import numpy as np
-from rdbl import gbp
 
-# generate a UK simulation from survey microdata
+sim = Microsimulation(mode="frs", year=2018)
 
-sim = PopulationSim()
-df = pd.DataFrame()
+df = sim.df(["national_insurance", "income_tax", "people"])
 
-# calculate each variable by specifying the name and time period
-
-# weights are needed to extrapolate to the UK population from
-# survey respondents
-
-df["Region"] = sim.calc("region", period="2020").decode_to_str()
-df["Population"] = sim.calc("people_in_household", period="2020") * sim.calc("household_weight", period="2020")
-df["National Insurance"] = sim.calc("NI", map_to="household", period="2020") * sim.calc("household_weight", period="2020")
-
-# sum over UK regions
-
-df = df.groupby("Region").sum()
-
-# calculate per-capita NI
-
-df["National Insurance per capita"] = df["National Insurance"] / df["Population"]
-
-# make figures readable and display results
-
-df["Population"] = df["Population"].apply(gbp)
-df["National Insurance"] = df["National Insurance"].apply(gbp)
-df["National Insurance per capita"] = df["National Insurance per capita"].apply(gbp)
-df.sort_values(by="National Insurance per capita")
+summary = pd.DataFrame(df.groupby(sim.calc("region", map_to="person")).sum()).sort_values(by="people", ascending=False)
+summary.national_insurance = summary.national_insurance.apply(lambda x: round(x / 1e+9, 1))
+summary.income_tax = summary.income_tax.apply(lambda x: round(x / 1e+9, 1))
+summary.people = summary.people.apply(lambda x: round(x / 1e+6, 1))
+summary.columns = ["National Insurance (£bn)", "Income Tax (£bn)", "Population (millions)"]
+summary
 
 
-```{toctree}
-:hidden:
-:titlesonly:
+# ### Reform evaluation
+# 
+# Below is an example of simulating the effects of a reform (namely, increasing the basic rate of income tax from 20% to 23%). This uses the SPI mode of input - using administrative tax data instead of the FRS household survey.
+
+# In[2]:
 
 
-Populations
-Individuals
-```
+from openfisca_uk.api import *
+
+def change_tax_parameters(parameters):
+    parameters.tax.income_tax.rates.uk.brackets[0].rate.update(
+        period=periods.period("year:2019:1"), value=0.23
+    )
+    return parameters
+
+class reform(Reform):
+    def apply(self):
+        self.modify_parameters(change_tax_parameters)
+
+baseline = Microsimulation(mode="spi")
+reformed = Microsimulation(reform, mode="spi")
+revenue = reformed.calc("tax").sum() - baseline.calc("tax").sum()
+f"Revenue: £{round(revenue / 1e+9, 1)}bn"
+
+
+# 
+# ```{toctree}
+# :hidden:
+# :titlesonly:
+# 
+# 
+# Reforms
+# Weighting
+# Individuals
+# ```
+# 
