bd4a4b107b195c595194d84de4259d0ff2ccc75e
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-07-07 12:25:13 +0100

    Add waterfall chart

--- openfisca_uk/graphs/population.py
+++ openfisca_uk/graphs/population.py
@@ -4,6 +4,7 @@ from openfisca_uk.api import *
 import pandas as pd
 from microdf import MicroSeries
 from functools import wraps
+from openfisca_uk.graphs.general import net_cost
 
 
 def _get_group_reduction(
@@ -34,7 +35,7 @@ def _get_group_reduction(
     return change
 
 
-def distributional_plot(
+def distributional_chart(
     incomes: MicroSeries,
     baseline_values: MicroSeries,
     reform_values: Union[MicroSeries, List[MicroSeries]],
@@ -133,23 +134,122 @@ def distributional_plot(
 
 # Bucket-specific shortcut functions
 
-quartile_plot = wraps(distributional_plot)(
-    lambda *args, **kwargs: distributional_plot(
+quartile_plot = wraps(distributional_chart)(
+    lambda *args, **kwargs: distributional_chart(
         *args, bucket="quartile", **kwargs
     )
 )
-quintile_plot = wraps(distributional_plot)(
-    lambda *args, **kwargs: distributional_plot(
+quintile_plot = wraps(distributional_chart)(
+    lambda *args, **kwargs: distributional_chart(
         *args, bucket="quintile", **kwargs
     )
 )
-decile_plot = wraps(distributional_plot)(
-    lambda *args, **kwargs: distributional_plot(
+decile_plot = wraps(distributional_chart)(
+    lambda *args, **kwargs: distributional_chart(
         *args, bucket="decile", **kwargs
     )
 )
-percentile_plot = wraps(distributional_plot)(
-    lambda *args, **kwargs: distributional_plot(
+percentile_plot = wraps(distributional_chart)(
+    lambda *args, **kwargs: distributional_chart(
         *args, bucket="percentile", **kwargs
     )
 )
+
+
+def waterfall_data(
+    reforms: List[Reform],
+    reform_labels: List[str],
+    subreform_labels: List[str],
+    baseline: Microsimulation = None,
+    **kwargs,
+):
+    """Generates data for a waterfall funding breakdown chart.
+
+    Args:
+        reforms (List[Reform]): A list of reforms to show funding breakdowns for. Each reform must have the same top-level subreforms.
+        reform_labels (List[str]): The names of the reforms.
+        subreform_labels (List[str]): The names of the components of the reforms.
+        baseline (Microsimulation, optional): A baseline simulation - can improve speed if already initialised. Defaults to None.
+
+    Returns:
+        pd.DataFrame: The waterfall plot dataframe.
+    """
+    baseline = baseline or Microsimulation(**kwargs)
+    dfs = []
+    for reform, name in zip(reforms, reform_labels):
+        net_costs = []
+        for i in range(len(reform) + 1):
+            net_costs += [
+                net_cost(baseline, Microsimulation(reform[:i], **kwargs))
+            ]
+        net_costs = np.array(net_costs)
+        labels = subreform_labels + ["Remaining"]
+        resulting_costs = pd.Series(net_costs[1:] - net_costs[:-1])
+        resulting_costs = pd.Series(list(resulting_costs) + [net_costs[-1]])
+        if net_costs[-1] < 0:
+            final_base = 0
+        else:
+            final_base = -net_costs[-1]
+        breakdown = pd.DataFrame(
+            dict(
+                # Each component has white below it, so double the records.
+                component=labels * 2,
+                amount=[0]
+                + list(-resulting_costs.cumsum()[1:-1])
+                + [final_base]
+                + list(resulting_costs.abs()),
+                Type=["-"] * len(labels)
+                + np.where(
+                    resulting_costs < 0, "Revenue", "Spending"
+                ).tolist(),
+                Reform=name,
+            )
+        )
+        dfs += [breakdown]
+    return pd.concat(dfs)
+
+
+SPENDING_COLOR = "#BDBDBD"
+REVENUE_COLOR = "#1976D2"
+
+
+def waterfall_chart(
+    reforms: List[Reform],
+    subreform_labels: List[str],
+    reform_labels: List[str] = None,
+    baseline: Microsimulation = None,
+):
+    """Generates a waterfull funding breakdown plot.
+
+    Args:
+        reforms (Union[Reform, List[Reform]]): A list of reforms - each must have the same top-level subreforms.
+        subreform_labels (List[str]): The names of the top-level reform components.
+        reform_labels (List[str], optional): The names of the reforms, if multiple are provided.
+        baseline (Microsimulation, optional): A baseline simulation - can improve speed if pre-initialised. Defaults to None.
+
+    Returns:
+        Figure: The Plotly figure.
+    """
+    if not isinstance(reforms, list):
+        reforms = [reforms]
+        reform_labels = ["Reform"]
+
+    return px.bar(
+        waterfall_data(
+            reforms, reform_labels, subreform_labels, baseline=baseline
+        ),
+        x="component",
+        y="amount",
+        color="Type",
+        barmode="stack",
+        animation_frame="Reform",
+        color_discrete_sequence=["white", REVENUE_COLOR, SPENDING_COLOR],
+    ).update_layout(
+        xaxis_title="Component",
+        yaxis_title="Surplus",
+        yaxis_tickprefix="£",
+        legend_title="",
+        # White background to match the unaffected area.
+        paper_bgcolor="rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)",
+    )
