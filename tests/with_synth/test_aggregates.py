0e412ead0433a86b0e131aeb20db966fb328053d
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-08-11 08:32:50 +0100

    Incorporate edits

--- tests/with_synth/test_aggregates.py
+++ tests/with_synth/test_aggregates.py
@@ -1,13 +1,13 @@
-from openfisca_uk_data import SynthFRS
+from openfisca_data import SynthFRS
 from openfisca_uk import Microsimulation
 import pytest
 
 sim = Microsimulation(dataset=SynthFRS)
 
-average_tests = (("in_poverty_bhc", 0.15, 0.1),)
+# (variable, population average, absolute error window)
+AVERAGE_TESTS = (("in_poverty_bhc", 0.15, 0.1),)
 
-
-@pytest.mark.parametrize("variable,central_estimate,max_dist", average_tests)
+@pytest.mark.parametrize("variable,central_estimate,max_dist", AVERAGE_TESTS)
 def test_person_level_averages(variable, central_estimate, max_dist):
     assert (
         abs(
@@ -18,14 +18,14 @@ def test_person_level_averages(variable, central_estimate, max_dist):
     )
 
 
-agg_tests = (
+AGG_TESTS = (
     ("tax", 223e9, 20e9),
     ("income_tax", 160e9, 20e9),
     ("benefits", 220e9, 30e9),
 )
 
 
-@pytest.mark.parametrize("variable,central_estimate,max_dist", agg_tests)
+@pytest.mark.parametrize("variable,central_estimate,max_dist", AGG_TESTS)
 def test_aggregates(variable, central_estimate, max_dist):
     assert (
         abs(sim.calc(variable, period=2020).sum() - central_estimate)
