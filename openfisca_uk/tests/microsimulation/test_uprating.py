880d28f4f82332698866ea7af5d30cbd2a7aa9a9
Nikhil <nikhil@policyengine.org>
2021-10-13 23:59:56 +0100

    Add uprating test

---
+++ openfisca_uk/tests/microsimulation/test_uprating.py
@@ -0,0 +1,23 @@
+from openfisca_uk import Microsimulation
+from openfisca_uk_data import FRS
+import pytest
+from itertools import product
+
+UPRATED_VARIABLES = (
+    "council_tax",
+    "employment_income",
+    "pension_income",
+    "self_employment_income",
+    "property_income",
+    "savings_interest_income",
+    "dividend_income",
+)
+
+sim = Microsimulation(dataset=FRS, year=2018)
+
+
+@pytest.mark.parametrize(
+    "variable,year", product(UPRATED_VARIABLES, range(2019, 2022))
+)
+def test_uprating(variable: str, year: int):
+    assert sim.calc(variable, year).sum() != sim.calc(variable, 2018).sum()
