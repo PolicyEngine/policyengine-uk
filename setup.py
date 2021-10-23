c21a277c236ee03777851ee4451605f8b2a03a1d
Nikhil <nikhil@policyengine.org>
2021-10-23 17:31:08 +0100

    Fix install action

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         "microdf_python>=0.3.0",
         "OpenFisca-UK-Data>=0.2.0,<0.3.0",
         # "PolicyEngine>=0.2.0,<0.3.0",
-        "PolicyEngine @ git+https://github.com/PolicyEngine/PolicyEngine-Core@server",
         "tqdm>=4.59.0",
         "plotly>=4.14.3",
         "argparse>=1.4.0",
