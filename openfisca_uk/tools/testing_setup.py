7cf8484f02582e710d273daabccf5b14c63bed53
Nikhil <nikhil@policyengine.org>
2021-10-24 11:41:15 +0100

    Add testing setup file

---
+++ openfisca_uk/tools/testing_setup.py
@@ -0,0 +1,7 @@
+from openfisca_uk_data import FRS, UKMODOutput
+from openfisca_uk.initial_setup import set_default
+
+FRS.download(2018)
+FRS.download(2019)
+UKMODOutput.download(2018)
+set_default(FRS)
