4e8f4aa96201dddfe3526610dcbf46fba11b6e1e
Nikhil <nikhil@policyengine.org>
2021-10-26 12:47:30 +0100

    Switch back to patch bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-UK",
-    version="0.6.0",
+    version="0.5.3",
     author="UBI Center",
     author_email="nikhil.woodruff@ubicenter.org",
     classifiers=[
