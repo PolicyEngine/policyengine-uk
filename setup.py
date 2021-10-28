72eff1643122ea19c12ac6bf3495f75acce21cc7
Max Ghenis <mghenis@gmail.com>
2021-10-27 22:01:45 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-UK",
-    version="0.5.5",
+    version="0.5.6",
     author="UBI Center",
     author_email="nikhil.woodruff@ubicenter.org",
     classifiers=[
