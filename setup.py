5352b6fea2c431e9e364f92262e8cb07c814936d
Nikhil <nikhil@policyengine.org>
2021-11-11 12:56:55 +0000

    Bump openfisca-uk-data requirement

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "pytest>=5.4.3",
         "OpenFisca-Core>=35.4.1",
         "microdf_python>=0.3.0",
-        "OpenFisca-UK-Data @ git+https://github.com/policyengine/openfisca-uk-data@bugfixes",
+        "OpenFisca-UK-Data>=0.3.1,<0.4.0",
         "OpenFisca-Tools>=0.1.3,<0.2.0",
         "tqdm>=4.59.0",
         "plotly>=4.14.3",
