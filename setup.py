fbe6c083f585f52bee9b41d185b41d8439019496
Nikhil <nikhil@policyengine.org>
2021-11-25 00:33:50 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-UK",
-    version="0.7.3",
+    version="0.7.4",
     author="PolicyEngine",
     author_email="nikhil@policyengine.org",
     classifiers=[
