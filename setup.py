8b47e6f551572bd4a5dd511193f8bf9731523949
Max Ghenis <mghenis@gmail.com>
2021-12-01 22:33:04 -0800

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-UK",
-    version="0.7.9",
+    version="0.7.10",
     author="PolicyEngine",
     author_email="nikhil@policyengine.org",
     classifiers=[
