ce355b4059585a96a43b3bd05c89418e396d9eca
Max Ghenis <mghenis@gmail.com>
2021-12-23 15:31:08 -0800

    Refactor formulas in class_2.py

--- openfisca_uk/variables/finance/tax/national_insurance/class_2.py
+++ openfisca_uk/variables/finance/tax/national_insurance/class_2.py
@@ -8,13 +8,13 @@ class weekly_NI_class_2(Variable):
     label = u"Class 2 Contributions for National Insurance"
     definition_period = YEAR
     reference = "Social Security and Benefits Act 1992 s. 11"
+    unit = "currency-GBP"
 
     def formula(person, period, parameters):
         class_2 = parameters(period).tax.national_insurance.class_2
         profits = person("self_employment_income", period)
         over_threshold = profits >= class_2.small_profits_threshold
-        charge = over_threshold * class_2.flat_rate * WEEKS_IN_YEAR
-        return charge
+        return over_threshold * class_2.flat_rate * WEEKS_IN_YEAR
 
 
 class NI_class_2(Variable):
@@ -22,6 +22,7 @@ class NI_class_2(Variable):
     entity = Person
     label = u"Class 2 Contributions for National Insurance for the year"
     definition_period = YEAR
+    unit = "currency-GBP"
 
     def formula(person, period, parameters):
         return person("weekly_NI_class_2", period)
