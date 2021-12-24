cee34af2ed1bc8d6d8bf7396b9d9a151fe884e99
Max Ghenis <mghenis@gmail.com>
2021-12-23 22:26:59 -0800

    refactor council_tax_benefit

--- openfisca_uk/variables/finance/benefit/family/council_tax_benefit.py
+++ openfisca_uk/variables/finance/benefit/family/council_tax_benefit.py
@@ -4,14 +4,15 @@ from openfisca_uk.model_api import *
 class council_tax_benefit_reported(Variable):
     value_type = float
     entity = Person
-    label = u"CTB (reported)"
+    label = u"Council Tax Benefit (reported)"
+    documentation = "Reported amount of Council Tax Benefit"
     definition_period = YEAR
 
 
 class council_tax_benefit(Variable):
     value_type = float
     entity = BenUnit
-    label = u"CTB"
+    label = u"Council Tax Benefit"
     definition_period = YEAR
 
     def formula(benunit, period, parameters):
