ecaee0c91844db326728a8da105fd6537a301c57
Max Ghenis <mghenis@gmail.com>
2021-12-23 22:47:23 -0800

    Refactor income_support

--- openfisca_uk/variables/finance/benefit/family/income_support.py
+++ openfisca_uk/variables/finance/benefit/family/income_support.py
@@ -60,20 +60,24 @@ class income_support_applicable_income(Variable):
         income -= aggr(benunit, period, ["pension_contributions"]) * 0.5
         family_type = benunit("family_type", period)
         families = family_type.possible_values
-        income = max_(
-            0,
-            income
-            - (family_type == families.SINGLE)
-            * IS.means_test.income_disregard_single
-            * WEEKS_IN_YEAR
-            - benunit("is_couple", period)
-            * IS.means_test.income_disregard_couple
-            * WEEKS_IN_YEAR
-            - (family_type == families.LONE_PARENT)
-            * IS.means_test.income_disregard_lone_parent
-            * WEEKS_IN_YEAR,
+        # Calculate income disregards for each family type.
+        mt = IS.means_test
+        single = family_type == families.SINGLE
+        income_disregard_single = single * mt.income_disregard_single
+        single = family_type == families.SINGLE
+        income_disregard_couple = (
+            benunit("is_couple", period) * mt.income_disregard_couple
         )
-        return income
+        lone_parent = family_type == families.LONE_PARENT
+        income_disregard_lone_parent = (
+            lone_parent * mt.income_disregard_lone_parent
+        )
+        income_disregard = (
+            income_disregard_single
+            + income_disregard_couple
+            + income_disregard_lone_parent
+        ) * WEEKS_IN_YEAR
+        return max_(0, income - income_disregard)
 
 
 class income_support_eligible(Variable):
@@ -83,16 +87,16 @@ class income_support_eligible(Variable):
     definition_period = YEAR
 
     def formula(benunit, period, parameters):
-        lone_parent = benunit("is_lone_parent", period) * (
-            benunit("youngest_child_age", period) <= 5
-        )
+        youngest_child_5_or_under = benunit("youngest_child_age", period) <= 5
+        lone_parent = benunit("is_lone_parent", period)
+        lone_parent_with_young_child = lone_parent & youngest_child_5_or_under
         QUALIFYING_COMPONENTS = ["is_carer_for_benefits"]
         eligible = (
-            aggr(benunit, period, QUALIFYING_COMPONENTS) + lone_parent
-        ) > 0
+            aggr(benunit, period, QUALIFYING_COMPONENTS) > 0
+        ) | lone_parent_with_young_child
         under_SP_age = benunit.any(benunit.members("is_SP_age", period)) == 0
         eligible *= under_SP_age
-        return not_(benunit("ESA_income", period) > 0) * eligible
+        return not_(benunit("ESA_income", period) > 0) & eligible
 
 
 class income_support_applicable_amount(Variable):
@@ -155,7 +159,4 @@ class income_support(Variable):
     def formula(benunit, period, parameters):
         amount = benunit("income_support_applicable_amount", period)
         income = benunit("income_support_applicable_income", period)
-        return max_(
-            0,
-            (amount - income),
-        )
+        return max_(0, amount - income)
