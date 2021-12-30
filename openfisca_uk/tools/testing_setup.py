fd71e14308a294f8cf0f474a8bc8fe64bda32511
Nikhil <nikhil@policyengine.org>
2021-12-30 00:25:16 +0000

    Set default dataset

--- openfisca_uk/tools/testing_setup.py
+++ openfisca_uk/tools/testing_setup.py
@@ -1,7 +1,7 @@
-from openfisca_uk_data import FRS, UKMODOutput, FRSEnhanced
+from openfisca_uk_data import UKMODOutput, FRSEnhanced
 from openfisca_uk.initial_setup import set_default
 
 FRSEnhanced.download(2018)
 FRSEnhanced.download(2019)
 UKMODOutput.download(2018)
-set_default(FRS)
+set_default(FRSEnhanced)
