bee9a397f9bbc66a763ccffec87b80071cd081c3
Nikhil <nikhil@policyengine.org>
2022-01-12 23:41:11 +0000

    Add SSMG

--- openfisca_uk/variables/finance/benefit/personal/maternity_allowance.py
+++ openfisca_uk/variables/finance/benefit/personal/maternity_allowance.py
@@ -7,3 +7,30 @@ class maternity_allowance_reported(Variable):
     label = "Maternity allowance"
     definition_period = YEAR
     unit = "currency-GBP"
+
+class maternity_allowance(Variable):
+    label = "Maternity Allowance"
+    entity = Person
+    definition_period = YEAR
+    value_type = float
+    unit = "currency-GBP"
+
+    def formula(person, period, parameters):
+        return person("maternity_allowance_reported", period)
+
+class ssmg_reported(Variable):
+    label = "Sure Start Maternity Grant (reported)"
+    entity = Person
+    definition_period = YEAR
+    value_type = float
+    unit = "currency-GBP"
+
+class ssmg(Variable):
+    label = "Sure Start Maternity Grant"
+    entity = Person
+    definition_period = YEAR
+    value_type = float
+    unit = "currency-GBP"
+
+    def formula(person, period, parameters):
+        return person("ssmg_reported", period)
