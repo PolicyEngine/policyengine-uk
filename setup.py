543e6bca8839b83b9eebfb2772a54b6cb15297f1
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-01-26 20:37:33 +0000

    Re-add fixes

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "OpenFisca-Core>=35.4.1",
         "microdf_python>=0.3.0",
         "OpenFisca-UK-Data>=0.7.1",
-        "OpenFisca-Tools>=0.2.1,<0.3.0",
+        "OpenFisca-Tools>=0.2.3,<0.3.0",
         "tqdm>=4.59.0",
         "plotly>=4.14.3",
         "argparse>=1.4.0",
