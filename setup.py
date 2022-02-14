02bbc4b5b7c80906ab74696948fb06f38208255f
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-02-14 22:51:45 +0000

    Versioning

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-UK",
-    version="0.12.1",
+    version="0.12.2",
     author="PolicyEngine",
     author_email="nikhil@policyengine.org",
     classifiers=[
