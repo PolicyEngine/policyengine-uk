Fix more validation leakage

openfisca_uk/calibration/losses/categories/council_tax_bands.py
@@ -20,10 +20,15 @@ class CouncilTaxBandHouseholds(LossCategory):
             for band in ct_bands.children[country].children:
                 parameter = ct_bands.children[country].children[band]
                 parameter_name = parameter.name + "." + str(year)
-                model_population = tf.reduce_sum(
+                people_in_households = (
                     (hh_country == country)
                     * (ct_band == band)
+                )
+                model_population = tf.reduce_sum(
+                    people_in_households
                     * household_weights
                 )
                 actual_population = parameter(f"{year}-01-01")
-                yield parameter_name, model_population, actual_population
+                if people_in_households.sum() > 0:
+                    # If the FRS has no observations, skip the target.
+                    yield parameter_name, model_population, actual_population

openfisca_uk/calibration/losses/categories/income_tax_revenue_by_income.py
@@ -5,7 +5,7 @@ from openfisca_uk.parameters import parameters
 
 
 class IncomeTaxRevenueByIncome(LossCategory):
-    weight = 1e3
+    weight = 1
     label = "Income tax revenue by income"
     parameter_folder = parameters.calibration.income_tax.revenue_by_income
 
@@ -14,7 +14,7 @@ class IncomeTaxRevenueByIncome(LossCategory):
         household_weights,
         year,
     ):
-        brackets = IncomeTaxRevenueByIncome.parameter_folder.brackets
+        brackets = IncomeTaxRevenueByIncome.parameter_folder.brackets[:-3] # Last-3 brackets are impossible due to FRS non-capture
         num_thresholds = len(brackets)
         instant_str = f"{year}-01-01"
         for i in range(num_thresholds):
@@ -36,14 +36,16 @@ class IncomeTaxRevenueByIncome(LossCategory):
             )
             aggregate = tf.reduce_sum(household_weights * household_income_tax)
             target = brackets[i].amount(instant_str)
-            yield brackets[i].name + "." + str(year), aggregate, target
+            if person_in_range.sum() > 0:
+                # If the FRS does not have any observations, skip the target.
+                yield brackets[i].name + "." + str(year), aggregate, target
 
     def get_metrics():
-        return IncomeTaxRevenueByIncome.parameter_folder.brackets
+        return IncomeTaxRevenueByIncome.parameter_folder.brackets[:-3]
 
     def get_metric_names():
         return [
             bracket.name + "." + str(year)
-            for bracket in IncomeTaxRevenueByIncome.parameter_folder.brackets
+            for bracket in IncomeTaxRevenueByIncome.parameter_folder.brackets[:-3]
             for year in range(2019, 2023)
         ]

openfisca_uk/calibration/losses/loss_calculator.py
@@ -76,7 +76,7 @@ class LossCalculator:
         adjusted_weights = tf.nn.relu(frs_weights + weight_changes)
         for loss_category in self.losses:
             frs_weights = self.sim.calc("household_weight").values
-            excluded_metrics = self.validation_metrics if validation else self.training_metrics
+            excluded_metrics = self.training_metrics if validation else self.validation_metrics
             loss_category_loss, loss_category_log = loss_category.compute(
                 self.sim,
                 adjusted_weights,