Fix validation leakage in IT-specific loss functions

openfisca_uk/calibration/losses/loss_calculator.py
@@ -66,7 +66,7 @@ class LossCalculator:
         Returns:
             tf.Tensor: The total loss.
         """
-        loss = 0
+        loss = tf.constant(0, dtype=tf.float32)
         frs_weights = np.array(
             [
                 self.sim.calc("household_weight", year).values
@@ -76,12 +76,11 @@ class LossCalculator:
         adjusted_weights = tf.nn.relu(frs_weights + weight_changes)
         for loss_category in self.losses:
             frs_weights = self.sim.calc("household_weight").values
+            excluded_metrics = self.validation_metrics if validation else self.training_metrics
             loss_category_loss, loss_category_log = loss_category.compute(
                 self.sim,
                 adjusted_weights,
-                self.training_metrics
-                if validation
-                else self.validation_metrics,
+                excluded_metrics=excluded_metrics,
             )
             loss += loss_category_loss
             self.training_log += [