8a64f739e291e3e645dd9e0b5a8a71e8feb40474
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-02-23 12:41:35 +0000

    Fix region/country error

--- openfisca_uk/calibration/losses/categories/tenure_type.py
+++ openfisca_uk/calibration/losses/categories/tenure_type.py
@@ -14,14 +14,14 @@ class TenureType(LossCategory):
         year,
     ):
         tenure_type = sim.calc("ons_tenure_type")
-        hh_region = sim.calc("region")
+        hh_country = sim.calc("country")
         tenure_metrics = TenureType.parameter_folder
-        for region in tenure_metrics.children:
-            for tenure in tenure_metrics.children[region].children:
-                parameter = tenure_metrics.children[region].children[tenure]
+        for country in tenure_metrics.children:
+            for tenure in tenure_metrics.children[country].children:
+                parameter = tenure_metrics.children[country].children[tenure]
                 parameter_name = parameter.name + "." + str(year)
                 model_population = tf.reduce_sum(
-                    (hh_region == region)
+                    (hh_country == country)
                     * (tenure_type == tenure)
                     * household_weights
                 )
