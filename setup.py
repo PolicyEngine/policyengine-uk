5debd698b27d0f38882d195883ec04c6a4052c36
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-03-05 17:11:24 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-UK",
-    version="0.14.1",
+    version="0.14.2",
     author="PolicyEngine",
     author_email="nikhil@policyengine.org",
     classifiers=[
