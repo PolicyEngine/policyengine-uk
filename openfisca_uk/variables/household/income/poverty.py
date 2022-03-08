1a694ed0d3391745af0ebc069762a42f9baa85c4
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-03-08 11:29:06 +0000

    Reorganise variables

--- openfisca_uk/variables/household/income/poverty.py
+++ openfisca_uk/variables/household/income/poverty.py
@@ -1,3 +1,4 @@
+from openfisca_uk_data.datasets import FRSEnhanced, SynthFRS
 from openfisca_uk.model_api import *
 
 
@@ -95,3 +96,66 @@ class poverty_gap_ahc(Variable):
     def formula(household, period, parameters):
         net_income = household("hbai_household_net_income_ahc", period)
         return max_(0, household("poverty_line_ahc", period) - net_income)
+
+
+class baseline_hbai_excluded_income(Variable):
+    label = "HBAI-excluded income (baseline)"
+    documentation = "Total value of income not included in HBAI household net income in the baseline"
+    entity = Household
+    definition_period = YEAR
+    value_type = float
+    unit = "currency-GBP"
+
+    def formula(household, period, parameters):
+        # Establish if currently running a microsimulation
+        if len(household.nb_persons()) > 1_000:
+            from openfisca_uk import Microsimulation
+
+            # Simulate baseline policy
+            dataset = FRSEnhanced if 2019 in FRSEnhanced.years else SynthFRS
+            result = Microsimulation(
+                dataset=dataset, year=2019
+            ).simulation.calculate("hbai_excluded_income", period)
+            # Check that the dataset/year combination is valid
+            # (i.e. that the arrays are the same size)
+            if len(result) == len(household.nb_persons()):
+                return result
+        # If baseline policy not viable from the above method,
+        # no change in HBAI excluded income
+        return household("hbai_excluded_income", period)
+
+
+class hbai_excluded_income(Variable):
+    label = "HBAI-excluded income"
+    documentation = (
+        "Total value of income not included in HBAI household net income"
+    )
+    entity = Household
+    definition_period = YEAR
+    value_type = float
+    unit = "currency-GBP"
+
+    def formula(household, period, parameters):
+        VARIABLES = [
+            "expected_sdlt",
+            "expected_ltt",
+            "expected_lbtt",
+            "business_rates",
+        ]
+        return -add(household, period, VARIABLES)
+
+
+class hbai_excluded_income_change(Variable):
+    label = "Change in HBAI-excluded income"
+    documentation = "Effect of policy reforms on HBAI-excluded income"
+    entity = Household
+    definition_period = YEAR
+    value_type = float
+    unit = "currency-GBP"
+
+    def formula(household, period, parameters):
+        hbai_excluded_income = household("hbai_excluded_income", period)
+        baseline_hbai_excluded_income = household(
+            "baseline_hbai_excluded_income", period
+        )
+        return hbai_excluded_income - baseline_hbai_excluded_income
