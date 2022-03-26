6f065c383f4310e18158aa5cf2acf47f68e1c016
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-03-26 16:12:56 +0000

    Add JSA parameters

--- openfisca_uk/tools/tax_benefit_uprating.py
+++ openfisca_uk/tools/tax_benefit_uprating.py
@@ -15,6 +15,12 @@ def add_tax_benefit_uprating(parameters: ParameterNode) -> ParameterNode:
                 "uprating.september_cpi",
                 data={
                     "values": data,
+                    "metadata": {
+                        "label": "Tax-benefit CPI",
+                        "unit": "/1",
+                        "name": "tax_benefit_cpi",
+                        "description": "CPI of the previous september (percentage of June 2015 levels). This uprates some (not all) tax-benefit parameters automatically."
+                    }
                 },
             ),
         )
