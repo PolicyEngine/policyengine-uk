fac2ea32b734a55fcd58bc78d7085e117d884e49
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-04-10 23:46:24 +0100

    Finish raw datasets

--- openfisca_uk/data/datasets/__init__.py
+++ openfisca_uk/data/datasets/__init__.py
@@ -1,3 +1,6 @@
 from openfisca_uk.data.datasets.frs import RawFRS, FRS
+from openfisca_uk.data.datasets.was import RawWAS
+from openfisca_uk.data.datasets.lcfs import RawLCFS
+from openfisca_uk.data.datasets.spi import RawSPI, SPI
 
-DATASETS = [RawFRS, FRS]
+DATASETS = [RawFRS, FRS, RawWAS, RawLCFS, RawSPI, SPI]
