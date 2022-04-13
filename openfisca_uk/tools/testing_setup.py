81c976b81dcbf38a1afc174f17975d5b9df07cfc
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-04-13 19:01:32 +0100

    Fix testing setup

--- openfisca_uk/tools/testing_setup.py
+++ openfisca_uk/tools/testing_setup.py
@@ -1,6 +1,6 @@
-from openfisca_uk_data import FRSEnhanced
+from openfisca_uk.data import EnhancedFRS, SynthFRS
 from openfisca_uk.initial_setup import set_default
 
-FRSEnhanced.download(2018)
-FRSEnhanced.download(2019)
-set_default(FRSEnhanced)
+EnhancedFRS.download(2022)
+SynthFRS.download(2022)
+set_default(EnhancedFRS)
