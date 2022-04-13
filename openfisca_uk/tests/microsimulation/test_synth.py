07e756fa765b4c0bf53a7490d5fc958a0203751a
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-04-13 17:54:54 +0100

    Fix errors

--- openfisca_uk/tests/microsimulation/test_synth.py
+++ openfisca_uk/tests/microsimulation/test_synth.py
@@ -1,6 +1,8 @@
 def test_synth_runs():
     from openfisca_uk import Microsimulation
-    from openfisca_uk_data import SynthFRS
+    from openfisca_uk.data import SynthFRS
+
+    
 
     sim = Microsimulation(dataset=SynthFRS)
     sim.calc("net_income")
