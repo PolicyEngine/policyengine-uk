a7ed5c6e847daf93f5928e54477a85d0d61f577e
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-04-24 20:09:53 +0100

    Recalibrate

---
+++ openfisca_uk/variables/household/demographic/original_weight.py
@@ -0,0 +1,7 @@
+from openfisca_uk.model_api import *
+
+class original_weight(Variable):
+    label = "Original FRS weight"
+    entity = Household
+    definition_period = YEAR
+    value_type = float
