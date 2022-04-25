7e57ead8d0857b13eef9b14bef19b0a10526db9b
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-04-25 20:01:11 +0100

    Rename single-variable files

--- openfisca_uk/variables/government/dwp/pension_credit/is_pension_credit_eligible.py
+++ openfisca_uk/variables/government/dwp/pension_credit/is_pension_credit_eligible.py
@@ -9,7 +9,7 @@ class is_pension_credit_eligible(Variable):
     unit = GBP
     reference = "https://www.legislation.gov.uk/ukpga/2002/16/section/1"
 
-    def formula_2002(benunit, period, parameters):
+    def formula(benunit, period, parameters):
         has_sp_age_member = benunit.any(benunit.members("is_SP_age", period))
         is_gc_eligible = benunit("is_guarantee_credit_eligible", period)
         is_sc_eligible = benunit("is_savings_credit_eligible", period)
