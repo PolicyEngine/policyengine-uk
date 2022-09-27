e0eb627a323fd2fa34496c4aaeead0a7da23c198
Max Ghenis <mghenis@gmail.com>
2022-09-26 19:24:27 -0700

    Add single UBI policy parameter Fixes #637

--- openfisca_uk/variables/contrib/ubi_center/basic_income/bi_maximum.py
+++ openfisca_uk/variables/contrib/ubi_center/basic_income/bi_maximum.py
@@ -10,17 +10,16 @@ class bi_maximum(Variable):
 
     def formula(person, period, parameters):
         bi = parameters(period).contrib.ubi_center.basic_income
+        weekly_flat_amount = bi.amount.flat
         is_senior_for_bi = person("is_SP_age", period)
         is_child_for_bi = person("age", period) < bi.amount.adult_age
         is_wa_for_bi = ~is_senior_for_bi & ~is_child_for_bi
-        return (
-            select(
-                [is_child_for_bi, is_wa_for_bi, is_senior_for_bi],
-                [
-                    bi.amount.by_age.child,
-                    bi.amount.by_age.working_age,
-                    bi.amount.by_age.senior,
-                ],
-            )
-            * WEEKS_IN_YEAR
+        weekly_amount_by_age = select(
+            [is_child_for_bi, is_wa_for_bi, is_senior_for_bi],
+            [
+                bi.amount.by_age.child,
+                bi.amount.by_age.working_age,
+                bi.amount.by_age.senior,
+            ],
         )
+        return (weekly_flat_amount + weekly_amount_by_age) * WEEKS_IN_YEAR
