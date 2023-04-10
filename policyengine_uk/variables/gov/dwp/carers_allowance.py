27732fc5ed0ffc4278f034cb86580d93d777e650
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-04-10 13:14:31 +0100

    Add parameter metadata

--- policyengine_uk/variables/gov/dwp/carers_allowance.py
+++ policyengine_uk/variables/gov/dwp/carers_allowance.py
@@ -10,8 +10,18 @@ class carers_allowance(Variable):
 
     def formula(person, period, parameters):
         receives_ca = person("carers_allowance_reported", period) > 0
-        rate = parameters(period).gov.dwp.carers_allowance.rate
-        return receives_ca * rate * WEEKS_IN_YEAR
+        rate = parameters(period).gov.dwp.carers_allowance
+        weekly_care_hours = person("care_hours", period)
+        meets_work_condition = weekly_care_hours >= rate.work_condition_hours
+        return (meets_work_condition | receives_ca) * rate * WEEKS_IN_YEAR
+    
+class care_hours(Variable):
+    label = "hours providing care"
+    documentation = "Weekly hours providing care to others"
+    entity = Person
+    definition_period = YEAR
+    value_type = float
+    unit = "hour"
 
 
 class carers_allowance_reported(Variable):
