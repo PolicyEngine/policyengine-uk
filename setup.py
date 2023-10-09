3011b412e809e5f85f988d82768556885182dd1b
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-10-09 18:33:50 +0100

    Remove numpy dep

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "gif[plotly]>=3.0.0",
         "inquirer>=2.7.0",
         "microdf_python>=0.3.0",
-        "numpy<1.22",
         "pandas",
         "plotly>=4.14.3",
         "PolicyEngine-Core>=2.1,<3",
