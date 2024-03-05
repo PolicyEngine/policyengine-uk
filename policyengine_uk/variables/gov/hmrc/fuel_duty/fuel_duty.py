eaa4a668da37db29afc9c2e4d323680082bc3de0
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-03-05 11:47:35 +0000

    Temporarily suspend fuel duty parameters

--- policyengine_uk/variables/gov/hmrc/fuel_duty/fuel_duty.py
+++ policyengine_uk/variables/gov/hmrc/fuel_duty/fuel_duty.py
@@ -8,14 +8,18 @@ ECONOMIC_CONSUMER_INCIDENCE = 1
 class fuel_duty(Variable):
     label = "Fuel duty (cars only)"
     entity = Household
-    definition_period = YEAR
+    definition_period = MONTH
     value_type = float
     unit = GBP
 
     def formula(household, period, parameters):
         fd = parameters(period).gov.hmrc.fuel_duty
-        petrol_litres = household("petrol_litres", period)
-        diesel_litres = household("diesel_litres", period)
+        petrol_litres = (
+            household("petrol_litres", period.this_year) / MONTHS_IN_YEAR
+        )
+        diesel_litres = (
+            household("diesel_litres", period.this_year) / MONTHS_IN_YEAR
+        )
         return (
             fd.petrol_and_diesel
             * (petrol_litres + diesel_litres)
