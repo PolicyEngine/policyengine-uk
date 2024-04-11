bd99affa6c79db93cdb1ef94b57b706f770f9028
Kittyaoooo <97534835+Kittyaoooo@users.noreply.github.com>
2024-04-11 11:46:49 -0400

    add care to learn parameters

--- policyengine_uk/parameters/gov/dfe/care_to_learn.py
+++ policyengine_uk/parameters/gov/dfe/care_to_learn.py
@@ -1,22 +1,18 @@
 from policyengine_uk.model_api import *
 
-class childcare(Variable): # rename to care_to_learn. also rename the file(.py) and move to a variables/dfe/care_to_learn.py location
-    label = "Care-to-learn subsidy" # "Care-to-learn subsidy" - more specific because there are a few different childcare subsidies
+class care_to_learn(Variable): 
+    label = "Care-to-learn subsidy" 
     documentation = "Childcare support for parents under 20 taking publicly-funded courses in England." # all good!
-    entity = Household # yep
-    definition_period = WEEK # change to MONTH and just make sure the month amounts are 4x the amount of the weekly values
+    entity = Household 
+    definition_period = MONTH
     value_type = float
     unit = GBP
-    reference = "https://www.gov.uk/care-to-learn/what-youll-get" # all good!
+    reference = "https://www.gov.uk/care-to-learn/what-youll-get" 
 
     def formula(household, period, parameters):
-        # MONTHLY example
-        # if it's £180 per week, the monthly value is just 180 * 52 / 12
-        live_in_london = household("live_in_london", period)
-        # live_in_london = household("region", period).decode_to_str() == "LONDON"
-        amount_per_child = parameters(period).gov.dwp.universal_credit.elements.childcare.childcare_amount_per_child
-        # amount_per_child = parameters(period).gov.dfe.care_to_learn.amount_per_child
+        live_in_london = household("region", period).decode_to_str() == "LONDON"
+        amount_per_child = parameters(period).gov.dfe.care_to_learn.amount_per_child
         if live_in_london:
-            return amount_per_child.IN_LONDON # multiply to get monthly amount
+            return amount_per_child.IN_LONDON 
         else:
-            return amount_per_child.OUTSIDE_LONDON # also multiply
+            return amount_per_child.OUTSIDE_LONDON 
