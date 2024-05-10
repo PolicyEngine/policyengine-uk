5dff8f52deb9df9800386b72d93457be93b3fa18
Kate Yao <kittyao923@gmail.com>
2024-05-10 18:13:52 -0400

    edit care-to-learn variable file for monthly value

--- policyengine_uk/variables/gov/dfe/care_to_learn.py
+++ policyengine_uk/variables/gov/dfe/care_to_learn.py
@@ -1,18 +1,18 @@
 from policyengine_uk.model_api import *
 
-class care_to_learn(Variable): 
-    label = "Care-to-learn subsidy" 
+class care_to_learn(Variable):
+    label = "Care-to-learn subsidy"
     documentation = "Childcare support for parents under 20 taking publicly-funded courses in England." # all good!
-    entity = Household 
+    entity = Household
     definition_period = MONTH
     value_type = float
     unit = GBP
-    reference = "https://www.gov.uk/care-to-learn/what-youll-get" 
+    reference = "https://www.gov.uk/care-to-learn/what-youll-get"
 
     def formula(household, period, parameters):
         live_in_london = household("region", period).decode_to_str() == "LONDON"
         amount_per_child = parameters(period).gov.dfe.care_to_learn.amount_per_child
         if live_in_london:
-            return amount_per_child.IN_LONDON 
+            return amount_per_child.IN_LONDON*4
         else:
-            return amount_per_child.OUTSIDE_LONDON 
+            return amount_per_child.OUTSIDE_LONDON*4
