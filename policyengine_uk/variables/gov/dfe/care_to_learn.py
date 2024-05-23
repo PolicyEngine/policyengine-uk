fbaa4e0a774387fa78540f4369e3c320c31ded10
Kate Yao <kittyao923@gmail.com>
2024-05-22 22:39:51 -0400

    add reference to amount_per_child and formula update for care to learn

--- policyengine_uk/variables/gov/dfe/care_to_learn.py
+++ policyengine_uk/variables/gov/dfe/care_to_learn.py
@@ -1,8 +1,9 @@
 from policyengine_uk.model_api import *
 
+
 class care_to_learn(Variable):
     label = "Care-to-learn subsidy"
-    documentation = "Childcare support for parents under 20 taking publicly-funded courses in England." # all good!
+    documentation = "Childcare support for parents under 20 taking publicly-funded courses in England."  # all good!
     entity = Household
     definition_period = MONTH
     value_type = float
@@ -10,9 +11,15 @@ class care_to_learn(Variable):
     reference = "https://www.gov.uk/care-to-learn/what-youll-get"
 
     def formula(household, period, parameters):
-        live_in_london = household("region", period).decode_to_str() == "LONDON"
-        amount_per_child = parameters(period).gov.dfe.care_to_learn.amount_per_child
-        if live_in_london:
-            return amount_per_child.IN_LONDON*4
-        else:
-            return amount_per_child.OUTSIDE_LONDON*4
+        live_in_london = (
+            household("region", period).decode_to_str() == "LONDON"
+        )
+        amount_per_child = parameters(
+            period
+        ).gov.dfe.care_to_learn.amount_per_child
+        multiplier = MONTHS_IN_YEAR / WEEKS_IN_YEAR
+        return where(
+            live_in_london,
+            amount_per_child.IN_LONDON * multiplier,
+            amount_per_child.OUTSIDE_LONDON * multiplier,
+        )
