42e20039c8b35dcb66053dd6e1d993b40654c385
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-05-27 23:27:26 +0100

    Fix Pensioner Personal Allowance #863 (#864)

--- policyengine_uk/variables/gov/hmrc/income_tax/allowances/allowances.py
+++ policyengine_uk/variables/gov/hmrc/income_tax/allowances/allowances.py
@@ -14,13 +14,20 @@ class personal_allowance(Variable):
     reference = "Income Tax Act 2007 s. 35"
 
     def formula(person, period, parameters):
-        PA = parameters(
-            period
-        ).gov.hmrc.income_tax.allowances.personal_allowance
+        params = parameters(period)
+        contrib = params.gov.contrib
+        PA = params.gov.hmrc.income_tax.allowances.personal_allowance
+        personal_allowance = PA.amount
+        sp_age = person("is_SP_age", period)
+        pensioner_pa = contrib.conservatives.pensioner_personal_allowance
+        if pensioner_pa != personal_allowance:
+            personal_allowance = where(
+                sp_age, pensioner_pa, personal_allowance
+            )
         ANI = person("adjusted_net_income", period)
         excess = max_(0, ANI - PA.maximum_ANI)
         reduction = excess * PA.reduction_rate
-        return max_(0, PA.amount - reduction)
+        return max_(0, personal_allowance - reduction)
 
 
 class blind_persons_allowance(Variable):
