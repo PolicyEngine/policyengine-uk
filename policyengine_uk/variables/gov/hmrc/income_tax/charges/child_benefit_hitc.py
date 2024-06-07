faa73d565985462bf46aed8d90e4a463708dd6f4
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-06-07 16:58:31 +0100

    Fix Reform option to phase out the CB HITC with household income #868

--- policyengine_uk/variables/gov/hmrc/income_tax/charges/child_benefit_hitc.py
+++ policyengine_uk/variables/gov/hmrc/income_tax/charges/child_benefit_hitc.py
@@ -13,10 +13,17 @@ class CB_HITC(Variable):
     def formula(person, period, parameters):
         CB_received = person.benunit("child_benefit", period)
         CB_HITC = parameters(period).gov.hmrc.income_tax.charges.CB_HITC
+        household_based = parameters(
+            period
+        ).gov.contrib.conservatives.cb_hitc_household
+        personal_income = person("adjusted_net_income", period)
+        if household_based:
+            income = person.benunit.sum(personal_income)
+        else:
+            income = personal_income
         percentage = (
             max_(
-                person("adjusted_net_income", period)
-                - CB_HITC.phase_out_start,
+                personal_income - CB_HITC.phase_out_start,
                 0,
             )
             # HITC is specified as a percent of Child Benefit recollected for
