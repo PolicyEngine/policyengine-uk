ae8fda3aca191881823f60ab91fca5f681dd34b0
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-06-10 23:06:15 +0100

    Versioning

--- policyengine_uk/reforms/policyengine/budget_change.py
+++ policyengine_uk/reforms/policyengine/budget_change.py
@@ -1,6 +1,122 @@
 from policyengine_uk.model_api import *
 
 
+class pre_budget_change_household_benefits(Variable):
+    value_type = float
+    entity = Household
+    label = "household benefits"
+    documentation = "Total value of benefits received by household"
+    definition_period = YEAR
+    unit = GBP
+    adds = [
+        "child_benefit",
+        "ESA_income",
+        "housing_benefit",
+        "income_support",
+        "JSA_income",
+        "pension_credit",
+        "universal_credit",
+        "working_tax_credit",
+        "child_tax_credit",
+        "attendance_allowance",
+        "AFCS",
+        "BSP",
+        "carers_allowance",
+        "dla",
+        "ESA_contrib",
+        "IIDB",
+        "incapacity_benefit",
+        "JSA_contrib",
+        "pip",
+        "sda",
+        "state_pension",
+        "maternity_allowance",
+        "SSP",
+        "SMP",
+        "ssmg",
+        "basic_income",
+        "epg_subsidy",
+        "cost_of_living_support_payment",
+        "energy_bills_rebate",
+    ]
+
+    def formula(household, period, parameters):
+        contrib = parameters(period).gov.contrib
+        uprating = contrib.benefit_uprating
+        benefits = pre_budget_change_household_benefits.adds
+        if contrib.abolish_council_tax:
+            benefits = [
+                benefit
+                for benefit in benefits
+                if benefit != "council_tax_benefit"
+            ]
+        general_benefits = add(
+            household,
+            period,
+            [
+                benefit
+                for benefit in benefits
+                if benefit not in ["basic_income"]
+            ],
+        )
+        non_sp_benefits = add(
+            household,
+            period,
+            [
+                benefit
+                for benefit in benefits
+                if benefit not in ["state_pension", "basic_income"]
+            ],
+        )
+        return (
+            add(household, period, benefits)
+            + general_benefits * uprating.all
+            + non_sp_benefits * uprating.non_sp
+        )
+
+
+class pre_budget_change_household_tax(Variable):
+    value_type = float
+    entity = Household
+    label = "household taxes"
+    documentation = "Total taxes owed by the household"
+    definition_period = YEAR
+    unit = GBP
+    adds = [
+        "change_in_expected_sdlt",
+        "change_in_expected_ltt",
+        "change_in_expected_lbtt",
+        "corporate_sdlt_change_incidence",
+        "business_rates_change_incidence",
+        "council_tax",
+        "domestic_rates",
+        "change_in_fuel_duty",
+        "tv_licence",
+        "wealth_tax",
+        "non_primary_residence_wealth_tax",
+        "income_tax",
+        "national_insurance",
+        "LVT",
+        "carbon_tax",
+        "vat_change",
+        "capital_gains_tax",
+    ]
+
+    def formula(household, period, parameters):
+        if parameters(period).gov.contrib.abolish_council_tax:
+            return add(
+                household,
+                period,
+                [
+                    tax
+                    for tax in pre_budget_change_household_tax.adds
+                    if tax not in ["council_tax"]
+                ],
+            )
+        else:
+            return add(household, period, pre_budget_change_household_tax.adds)
+
+
 class pre_budget_change_household_net_income(Variable):
     label = "household net income"
     documentation = "household net income"
@@ -8,8 +124,8 @@ class pre_budget_change_household_net_income(Variable):
     definition_period = YEAR
     value_type = float
     unit = GBP
-    adds = ["household_market_income", "household_benefits"]
-    subtracts = ["household_tax"]
+    adds = ["household_market_income", "pre_budget_change_household_benefits"]
+    subtracts = ["pre_budget_change_household_tax"]
 
 
 class pre_budget_change_ons_household_income_decile(Variable):
@@ -60,12 +176,10 @@ class nhs_budget_change(Variable):
         decile = pd.Series(decile)
 
         budget_increase = (
-            parameters(period).gov.contrib.policyengine.nhs
-            * 1e9
+            parameters(period).gov.contrib.policyengine.budget.nhs * 1e9
         )
         budget_increase_per_decile = {
-            i: budget_increase * DECILE_INCIDENCE[i]
-            for i in range(1, 11)
+            i: budget_increase * DECILE_INCIDENCE[i] for i in range(1, 11)
         }
         households_per_decile = (
             pd.Series(weight).groupby(decile).sum().to_dict()
@@ -77,7 +191,6 @@ class nhs_budget_change(Variable):
         }
 
         return decile.map(average_per_decile).replace({np.nan: 0})
-    
 
 
 class education_budget_change(Variable):
@@ -107,12 +220,10 @@ class education_budget_change(Variable):
         decile = pd.Series(decile)
 
         budget_increase = (
-            parameters(period).gov.contrib.policyengine.nhs
-            * 1e9
+            parameters(period).gov.contrib.policyengine.budget.education * 1e9
         )
         budget_increase_per_decile = {
-            i: budget_increase * DECILE_INCIDENCE[i]
-            for i in range(1, 11)
+            i: budget_increase * DECILE_INCIDENCE[i] for i in range(1, 11)
         }
         households_per_decile = (
             pd.Series(weight).groupby(decile).sum().to_dict()
@@ -124,8 +235,6 @@ class education_budget_change(Variable):
         }
 
         return decile.map(average_per_decile).replace({np.nan: 0})
-
-
 
 
 class other_public_spending_budget_change(Variable):
@@ -155,12 +264,13 @@ class other_public_spending_budget_change(Variable):
         decile = pd.Series(decile)
 
         budget_increase = (
-            parameters(period).gov.contrib.policyengine.nhs
+            parameters(
+                period
+            ).gov.contrib.policyengine.budget.other_public_spending
             * 1e9
         )
         budget_increase_per_decile = {
-            i: budget_increase * DECILE_INCIDENCE[i]
-            for i in range(1, 11)
+            i: budget_increase * DECILE_INCIDENCE[i] for i in range(1, 11)
         }
         households_per_decile = (
             pd.Series(weight).groupby(decile).sum().to_dict()
@@ -174,45 +284,187 @@ class other_public_spending_budget_change(Variable):
         return decile.map(average_per_decile).replace({np.nan: 0})
 
 
-class household_net_income(Variable):
-    label = "household net income"
-    documentation = "household net income"
-    entity = Household
-    definition_period = YEAR
+class household_tax(Variable):
     value_type = float
+    entity = Household
+    label = "household taxes"
+    documentation = "Total taxes owed by the household"
+    definition_period = YEAR
     unit = GBP
     adds = [
-        "household_market_income",
-        "household_benefits",
+        "change_in_expected_sdlt",
+        "change_in_expected_ltt",
+        "change_in_expected_lbtt",
+        "corporate_sdlt_change_incidence",
+        "business_rates_change_incidence",
+        "council_tax",
+        "domestic_rates",
+        "change_in_fuel_duty",
+        "tv_licence",
+        "wealth_tax",
+        "non_primary_residence_wealth_tax",
+        "income_tax",
+        "national_insurance",
+        "LVT",
+        "carbon_tax",
+        "vat_change",
+        "capital_gains_tax",
+        "corporate_incident_tax_revenue_change",
+        "consumer_incident_tax_revenue_change",
     ]
-    subtracts = ["household_tax", "direct_tax_budget_change"]
+
+    def formula(household, period, parameters):
+        if parameters(period).gov.contrib.abolish_council_tax:
+            return add(
+                household,
+                period,
+                [
+                    tax
+                    for tax in household_tax.adds
+                    if tax not in ["council_tax"]
+                ],
+            )
+        else:
+            return add(household, period, household_tax.adds)
 
 
-class hbai_household_net_income(Variable):
+class household_benefits(Variable):
     value_type = float
     entity = Household
-    label = "Household net income (HBAI definition)"
-    documentation = "Disposable income for the household, following the definition used for official poverty statistics"
-    unit = GBP
+    label = "household benefits"
+    documentation = "Total value of benefits received by household"
     definition_period = YEAR
+    unit = GBP
+    adds = [
+        "child_benefit",
+        "ESA_income",
+        "housing_benefit",
+        "income_support",
+        "JSA_income",
+        "pension_credit",
+        "universal_credit",
+        "working_tax_credit",
+        "child_tax_credit",
+        "attendance_allowance",
+        "AFCS",
+        "BSP",
+        "carers_allowance",
+        "dla",
+        "ESA_contrib",
+        "IIDB",
+        "incapacity_benefit",
+        "JSA_contrib",
+        "pip",
+        "sda",
+        "state_pension",
+        "maternity_allowance",
+        "SSP",
+        "SMP",
+        "ssmg",
+        "basic_income",
+        "epg_subsidy",
+        "cost_of_living_support_payment",
+        "energy_bills_rebate",
+        "nhs_budget_change",
+        "education_budget_change",
+        "other_public_spending_budget_change",
+    ]
 
-    adds = ["household_gross_income"]
-    subtracts = ["household_tax", "baseline_hbai_excluded_income", "direct_tax_budget_change"]
+    def formula(household, period, parameters):
+        contrib = parameters(period).gov.contrib
+        uprating = contrib.benefit_uprating
+        benefits = household_benefits.adds
+        if contrib.abolish_council_tax:
+            benefits = [
+                benefit
+                for benefit in benefits
+                if benefit != "council_tax_benefit"
+            ]
+        general_benefits = add(
+            household,
+            period,
+            [
+                benefit
+                for benefit in benefits
+                if benefit not in ["basic_income"]
+            ],
+        )
+        non_sp_benefits = add(
+            household,
+            period,
+            [
+                benefit
+                for benefit in benefits
+                if benefit not in ["state_pension", "basic_income"]
+            ],
+        )
+        return (
+            add(household, period, benefits)
+            + general_benefits * uprating.all
+            + non_sp_benefits * uprating.non_sp
+        )
+
+
+class corporate_incident_tax_revenue_change(Variable):
+    label = "corporate-incident tax revenue change"
+    entity = Household
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+
+    def formula(household, period, parameters):
+        share = household("shareholding", period)
+        revenue_change = parameters(
+            period
+        ).gov.contrib.policyengine.budget.corporate_incident_tax_change
+        return revenue_change * share * 1e9
+
+
+class consumer_incident_tax_revenue_change(Variable):
+    label = "consumer-incident tax revenue change"
+    entity = Household
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+
+    def formula(household, period, parameters):
+        consumption = household("consumption", period)
+        if (
+            hasattr(household.simulation, "dataset")
+            and household("consumption", period).sum() != 0
+        ):
+            weight = household("household_weight", period)
+            share = consumption / (consumption * weight).sum()
+        else:
+            share = consumption / 846e9
+        revenue_change = parameters(
+            period
+        ).gov.contrib.policyengine.budget.consumer_incident_tax_change
+        return revenue_change * share * 1e9
 
 
 class budget_change_reform(Reform):
     def apply(self):
         self.add_variables(
             pre_budget_change_ons_household_income_decile,
+            pre_budget_change_household_benefits,
+            pre_budget_change_household_tax,
             pre_budget_change_household_net_income,
-            direct_tax_budget_change,
+            nhs_budget_change,
+            education_budget_change,
+            other_public_spending_budget_change,
+            corporate_incident_tax_revenue_change,
+            consumer_incident_tax_revenue_change,
         )
-        self.update_variable(household_net_income)
-        self.update_variable(hbai_household_net_income)
+        self.update_variable(household_benefits)
+        self.update_variable(household_tax)
 
 
-def create_budget_change_reform(parameters, period):
-    if parameters(period).gov.contrib.ons.etb.direct_tax_budget_change == 0:
-        return
+def create_budget_change_reform(parameters, _):
+    budget = parameters.gov.contrib.policyengine.budget
+    for child in budget.children:
+        for iteration in budget.children[child].values_list:
+            if iteration.value != 0:
+                return budget_change_reform
 
-    return budget_change_reform
+    return None
