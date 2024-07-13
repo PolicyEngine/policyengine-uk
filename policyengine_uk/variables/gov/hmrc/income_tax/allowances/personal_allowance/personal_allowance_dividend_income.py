8cb73f75fefde033ed2ada2bb1936b87bbbf577b
Anthony Volk <anthony@policyengine.org>
2024-07-12 23:21:53 -0400

    fix: Repair tests

--- policyengine_uk/variables/gov/hmrc/income_tax/allowances/personal_allowance/personal_allowance_dividend_income.py
+++ policyengine_uk/variables/gov/hmrc/income_tax/allowances/personal_allowance/personal_allowance_dividend_income.py
@@ -10,8 +10,16 @@ class personal_allowance_dividend_income(Variable):
     reference = "Income Tax Act 2007 s. 35"
 
     def formula(person, period, parameters):
-        max_pa = person("personal_allowance_savings_income", period)
+        pa_max_value = person("personal_allowance_max_value", period)
+        pa_earned_income = person(
+            "personal_allowance_earned_taxable_income", period
+        )
+        pa_savings_income = person("personal_allowance_savings_income", period)
+        pa_remaining = pa_max_value - pa_earned_income - pa_savings_income
 
-        income = person("taxable_dividend_income", period)
+        dividend_income = person("taxable_dividend_income", period)
+        is_income_lesser = dividend_income < pa_remaining
 
-        return max_(0, max_pa - income)
+        return where(
+            is_income_lesser, max_(0, dividend_income), max_(0, pa_remaining)
+        )
