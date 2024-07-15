d1b560c14723e74a072d4e8aac9c4b37d58ac529
Anthony Volk <anthony@policyengine.org>
2024-07-15 17:07:56 -0400

    fix: Handle cases where dividend income is covered by Personal Allowance

--- policyengine_uk/variables/gov/hmrc/income_tax/allowances/dividend_allowance.py
+++ policyengine_uk/variables/gov/hmrc/income_tax/allowances/dividend_allowance.py
@@ -11,16 +11,17 @@ class dividend_allowance(Variable):
 
     def formula(person, period, parameters):
         # Take maximum allowance
-        max_allowance = parameters(period).gov.hmrc.income_tax.allowances.dividend_allowance
+        max_allowance = parameters(
+            period
+        ).gov.hmrc.income_tax.allowances.dividend_allowance
 
         # Find remaining coverable dividend income
-        remaining_dividend_income = person("taxable_dividend_income", period) - person("personal_allowance_dividend_income", period)
+        remaining_dividend_income = person(
+            "taxable_dividend_income", period
+        ) - person("personal_allowance_dividend_income", period)
 
         # Find eligible amount
         calculated_amount = min_(max_allowance, remaining_dividend_income)
 
         # Ensure negatives return 0
-        return max_(
-            0,
-            calculated_amount
-        )
+        return max_(0, calculated_amount)
