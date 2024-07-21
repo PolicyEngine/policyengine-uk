371f504e0c5f1b44e83f0310da1f19a9dd70ede7
PavelMakarchuk <pavel.ma99@gmail.com>
2024-07-22 00:35:31 +0200

    minor

--- policyengine_uk/variables/gov/dwp/pension_credit/guarantee_credit/minimum_guarantee/additional/child/pension_credit_child_minimum_guarantee_addition.py
+++ policyengine_uk/variables/gov/dwp/pension_credit/guarantee_credit/minimum_guarantee/additional/child/pension_credit_child_minimum_guarantee_addition.py
@@ -12,8 +12,8 @@ class pension_credit_child_minimum_guarantee_addition(Variable):
     def formula(benunit, period, parameters):
         person = benunit.members
         is_child = person("is_child_or_QYP", period)
-        gc = parameters(period).gov.dwp.pension_credit.guarantee_credit
+        gc = parameters(period).gov.dwp.pension_credit.guarantee_credit.child
 
-        base_amount = benunit.sum(is_child) * gc.child.amount * WEEKS_IN_YEAR
+        base_amount = benunit.sum(is_child) * gc.amount * WEEKS_IN_YEAR
         disabled_child_amount = benunit("pension_credit_disabled_child_minimum_guarantee_addition", period)
         return base_amount + disabled_child_amount
