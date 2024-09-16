a448f8526b2f25eebac0a050f650271557b48075
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-09-16 11:17:29 +0100

    Fix housing benefit bug

--- policyengine_uk/variables/gov/dwp/housing_benefit/housing_benefit_pre_benefit_cap.py
+++ policyengine_uk/variables/gov/dwp/housing_benefit/housing_benefit_pre_benefit_cap.py
@@ -7,7 +7,8 @@ class housing_benefit_pre_benefit_cap(Variable):
     label = "Housing Benefit pre-benefit cap"
     definition_period = YEAR
     unit = GBP
-    defined_for = "housing_benefit_eligible"
-    defined_for = "would_claim_housing_benefit"
 
-    adds = ["housing_benefit_entitlement"]
+    def formula(benunit, period, parameters):
+        eligible = benunit("housing_benefit_eligible", period)
+        would_claim = benunit("would_claim_housing_benefit", period)
+        return where(eligible & would_claim, benunit("housing_benefit_entitlement", period), 0)
