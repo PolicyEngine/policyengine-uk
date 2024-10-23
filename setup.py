5685b7e7e7ca45524752cab67430cf66c1dfb0ee
Vahid Ahmadi <57112303+vahid-ahmadi@users.noreply.github.com>
2024-10-23 10:22:00 +0100

    Uprating benefit levels for 2025/26 (#974)
    * uprating-benefits2025
    
    * double-checking
    
    * Minor chnages to economy: false labels
    
    * Versioning
    
    ---------
    
    Co-authored-by: Nikhil Woodruff <nikhil.woodruff@outlook.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PolicyEngine-UK",
-    version="2.10.0",
+    version="2.11.0",
     author="PolicyEngine",
     author_email="nikhil@policyengine.org",
     classifiers=[
