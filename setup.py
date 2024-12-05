b493b3b5db283a131d670c947ecef5c54bdb97e0
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-12-05 12:43:17 +0000

    Versioning

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PolicyEngine-UK",
-    version="2.17.0",
+    version="2.18.0",
     author="PolicyEngine",
     author_email="nikhil@policyengine.org",
     classifiers=[
