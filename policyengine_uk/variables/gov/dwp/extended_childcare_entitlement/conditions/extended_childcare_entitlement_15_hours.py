014f8d35511818e8ab09ad7c02fcad144b63ebbd
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-01-22 22:09:39 +0000

    Edit labels

--- policyengine_uk/variables/gov/dwp/extended_childcare_entitlement/conditions/extended_childcare_entitlement_15_hours.py
+++ policyengine_uk/variables/gov/dwp/extended_childcare_entitlement/conditions/extended_childcare_entitlement_15_hours.py
@@ -1,16 +1,16 @@
 from policyengine_uk.model_api import *
 
 
-class free_childcare_15_hours(Variable):
+class extended_childcare_entitlement_15_hours(Variable):
     value_type = bool
     entity = Person
     label = "Free childcare 15 hours eligibility"
-    documentation = "Whether this child meets the age requirements for 15 hours of free childcare"
+    documentation = "Whether this child meets the age requirements for 15 hours of extended childcare entitlement"
     definition_period = YEAR  # Evaluate eligibility on a yearly basis
 
     def formula(person, period, parameters):
         """
-        Determine if a child is eligible for 15 hours of free childcare based on age.
+        Determine if a child is eligible for 15 hours of extended childcare entitlement based on age.
 
         Returns:
             bool: True if the child is eligible for 15 hours, False otherwise.
@@ -18,16 +18,16 @@ class free_childcare_15_hours(Variable):
         # Get the child's age
         age = person("age", period)
 
-        # Retrieve age thresholds for 15 hours eligibility
+        # Retrieve age thresholds
         age_limits = parameters(
             period
         ).gov.dwp.extended_childcare_entitlement.age
-        lower_limit_9_months = age_limits.lower_limit_9_months
-        upper_limit_3_years = age_limits.upper_limit_3_years
+        lower_limit = age_limits.lower_limit_for_first_entitlement
+        upper_limit = age_limits.upper_limit_for_first_entitlement
 
-        # Check if the child is eligible for 15 hours (between 3 and 4 years old)
-        eligible_15_hours = (age >= lower_limit_9_months) & (
-            age <= upper_limit_3_years
+        # Check if the child is eligible
+        eligible_15_hours = (age >= lower_limit) & (
+            age <= upper_limit
         )
 
         # Return the eligibility status
