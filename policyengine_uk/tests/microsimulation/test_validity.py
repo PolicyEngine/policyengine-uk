f65d76548a52df039a4627f3188bcc34f8d773bd
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-02-03 11:21:55 +0000

    Just get the stack trace

--- policyengine_uk/tests/microsimulation/test_validity.py
+++ policyengine_uk/tests/microsimulation/test_validity.py
@@ -2,11 +2,11 @@ from policyengine import Simulation
 import pytest
 
 YEARS = range(2024, 2026)
-baseline = Simulation({"scope": "macro", "country": "uk"}).baseline_simulation
 
 
 @pytest.mark.parametrize("year", YEARS)
 def test_not_nan(year):
+    baseline = Simulation({"scope": "macro", "country": "uk"}).baseline_simulation
     for variable in baseline.tax_benefit_system.variables:
         requires_computation_after = baseline.tax_benefit_system.variables[
             variable
