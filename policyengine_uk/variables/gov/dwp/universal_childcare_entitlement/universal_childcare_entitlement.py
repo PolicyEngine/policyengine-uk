30e4376a60b4bf680b8e68d78f32c106540e1b7f
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-02-12 13:14:25 +0100

    Edit based on chat-gpt comments

--- policyengine_uk/variables/gov/dwp/universal_childcare_entitlement/universal_childcare_entitlement.py
+++ policyengine_uk/variables/gov/dwp/universal_childcare_entitlement/universal_childcare_entitlement.py
@@ -12,9 +12,13 @@ class universal_childcare_entitlement(Variable):
     def formula(person, period, parameters):
         # Get the hours entitled for this child
         hours = person("universal_childcare_entitlement_hours", period)
+        hours = where(hours < 0, 0, hours)
 
         # Get the funding rate from parameters
         p = parameters(period).gov.dwp.universal_childcare_entitlement
+        rate = p.hourly_fund_rate
 
-        # Calculate total funding amount (hours * rate)
-        return hours * p.fund_rate
+        # Calculate total funding amount
+        total_entitlement = hours * rate
+
+        return total_entitlement
