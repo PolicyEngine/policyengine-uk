8de39999370d01ccbed9495325a4e99e19a99e78
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-02-13 18:41:18 +0100

    Merge hours and expenses py files

--- policyengine_uk/variables/gov/dfe/extended_childcare_entitlement/extended_childcare_entitlement.py
+++ policyengine_uk/variables/gov/dfe/extended_childcare_entitlement/extended_childcare_entitlement.py
@@ -5,22 +5,18 @@ class extended_childcare_entitlement(Variable):
     value_type = float
     entity = BenUnit
     label = "Annual extended childcare entitlement expenses"
-    documentation = (
-        "Annual expenses for extended childcare entitlement calculated as "
-        "hours per week * hourly rate * weeks per year"
-    )
     definition_period = YEAR
     unit = GBP
+    defined_for = "extended_childcare_entitlement_eligible"
 
     def formula(benunit, period, parameters):
-        # Get weekly hours
-        weekly_hours_per_child = benunit.members(
-            "extended_childcare_entitlement_hours", period
-        )
 
-        # Get expense rate from parameters
+        # Get parameters
         p = parameters(period).gov.dfe.extended_childcare_entitlement
         age = benunit.members("age", period)
+
+        # Compute weekly hours directly inside this function
+        weekly_hours_per_child = p.hours.calc(age)
 
         # Compute subsidy per child
         subsidy_per_child = weekly_hours_per_child * p.expense_rate.calc(age)
