464b4374fbbfdd19ddf79c6f47b81abf88b203e1
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-02-17 11:04:37 +0000

    Edit benunit level returns

--- policyengine_uk/variables/gov/dwp/targeted_childcare_entitlement/targeted_childcare_entitlement.py
+++ policyengine_uk/variables/gov/dwp/targeted_childcare_entitlement/targeted_childcare_entitlement.py
@@ -3,14 +3,13 @@ from policyengine_uk.model_api import *
 
 class targeted_childcare_entitlement(Variable):
     value_type = float
-    entity = Person
+    entity = BenUnit
     label = "targeted childcare entitlement amount per year"
     definition_period = YEAR
     unit = "currency-GBP"
 
-    def formula(person, period, parameters):
-        # Get the hours entitled for this child
-        hours = person("targeted_childcare_entitlement_hours", period)
-
+    def formula(benunit, period, parameters):
+        hours = benunit.members("targeted_childcare_entitlement_hours", period)
         p = parameters(period).gov.dwp.targeted_childcare_entitlement
-        return hours * p.funding_rate
+        individual_subsidy = hours * p.funding_rate
+        return benunit.sum(individual_subsidy)
