8838ea90796e8c1070e173cff9509b1e5d0f1540
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-02-19 19:16:12 +0000

    Change to report at person level

--- policyengine_uk/variables/gov/dfe/universal_childcare_entitlement/universal_childcare_entitlement.py
+++ policyengine_uk/variables/gov/dfe/universal_childcare_entitlement/universal_childcare_entitlement.py
@@ -3,16 +3,14 @@ from policyengine_uk.model_api import *
 
 class universal_childcare_entitlement(Variable):
     value_type = float
-    entity = BenUnit
+    entity = Person
+    defined_for = "universal_childcare_entitlement_eligible"
     label = "universal childcare entitlement amount per year"
     definition_period = YEAR
     unit = "currency-GBP"
 
-    def formula(benunit, period, parameters):
+    def formula(person, period, parameters):
         p = parameters(period).gov.dfe
-        is_eligible = benunit.members(
-            "universal_childcare_entitlement_eligible", period
-        )
-        ages = benunit.members("age", period)
-        hours = where(is_eligible, p.universal_childcare_entitlement.hours, 0)
-        return benunit.sum(hours * p.childcare_funding_rate.calc(ages))
+        age = person("age", period)
+        hours = p.universal_childcare_entitlement.hours
+        return hours * p.childcare_funding_rate.calc(age)
