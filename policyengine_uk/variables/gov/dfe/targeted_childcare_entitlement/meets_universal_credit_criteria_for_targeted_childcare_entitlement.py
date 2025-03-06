1d35929eeb2574d28886c8e6cd5f69959d27e96d
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-03-06 13:09:03 +0100

    Make format

--- policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/meets_universal_credit_criteria_for_targeted_childcare_entitlement.py
+++ policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/meets_universal_credit_criteria_for_targeted_childcare_entitlement.py
@@ -1,22 +1,27 @@
 from policyengine_uk.model_api import *
 
-class meets_universal_credit_criteria_for_targeted_childcare_entitlement(Variable):
+
+class meets_universal_credit_criteria_for_targeted_childcare_entitlement(
+    Variable
+):
     value_type = bool
     entity = BenUnit
-    label = "Meets Universal Credit criteria for targeted childcare entitlement"
+    label = (
+        "Meets Universal Credit criteria for targeted childcare entitlement"
+    )
     definition_period = YEAR
-    
+
     def formula(benunit, period, parameters):
         p = parameters(period).gov.dfe.targeted_childcare_entitlement
-        
+
         # Check if receiving Universal Credit
         uc = benunit("universal_credit", period)
 
         # For Universal Credit recipients, we must use the definition of "earned income"
         # The Local Authority (Duty to Secure Early Years Provision Free of Charge) (Amendment) Regulations 2018 - part 2.c
         # https://www.legislation.gov.uk/uksi/2018/146/made
-        
+
         # Check if earned income is below threshold
         earned_income = benunit.sum(benunit.members("earned_income", period))
-        
+
         return (uc > 0) & (earned_income <= p.max_income_uc_recipients)
--- policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/targeted_childcare_entitlement_eligible.py
+++ policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/targeted_childcare_entitlement_eligible.py
@@ -1,33 +1,39 @@
 from policyengine_uk.model_api import *
 
+
 class targeted_childcare_entitlement_eligible(Variable):
     value_type = bool
     entity = BenUnit
     label = "eligibility for targeted childcare entitlement"
     definition_period = YEAR
-    
+
     adds = [
-        "meets_universal_credit_criteria_for_targeted_childcare_entitlement", 
-        "meets_tax_credit_criteria_for_targeted_childcare_entitlement"
+        "meets_universal_credit_criteria_for_targeted_childcare_entitlement",
+        "meets_tax_credit_criteria_for_targeted_childcare_entitlement",
     ]
-    
+
     def formula(benunit, period, parameters):
         # Check if household is in England
         country = benunit.household("country", period)
         in_england = country == country.possible_values.ENGLAND
-        
+
         # Check other qualifying benefits
         p = parameters(period).gov.dfe.targeted_childcare_entitlement
-        has_qualifying_benefits = add(benunit, period, p.qualifying_benefits) > 0
-        
+        has_qualifying_benefits = (
+            add(benunit, period, p.qualifying_benefits) > 0
+        )
+
         # Use the variable names directly instead of self.adds
-        meets_any_criteria = add(
-            benunit, 
-            period, 
-            [
-                "meets_universal_credit_criteria_for_targeted_childcare_entitlement",
-                "meets_tax_credit_criteria_for_targeted_childcare_entitlement"
-            ]
-        ) > 0
-        
+        meets_any_criteria = (
+            add(
+                benunit,
+                period,
+                [
+                    "meets_universal_credit_criteria_for_targeted_childcare_entitlement",
+                    "meets_tax_credit_criteria_for_targeted_childcare_entitlement",
+                ],
+            )
+            > 0
+        )
+
         return (meets_any_criteria | has_qualifying_benefits) & in_england
