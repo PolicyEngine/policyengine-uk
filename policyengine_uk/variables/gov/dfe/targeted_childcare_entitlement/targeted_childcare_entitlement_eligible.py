014f33701aa8d233f0216b9fc109b3cfa2016035
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-03-06 13:08:52 +0100

    Apply Max's comment

--- policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/targeted_childcare_entitlement_eligible.py
+++ policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/targeted_childcare_entitlement_eligible.py
@@ -1,57 +1,33 @@
 from policyengine_uk.model_api import *
 
-
 class targeted_childcare_entitlement_eligible(Variable):
     value_type = bool
     entity = BenUnit
     label = "eligibility for targeted childcare entitlement"
     definition_period = YEAR
-
+    
+    adds = [
+        "meets_universal_credit_criteria_for_targeted_childcare_entitlement", 
+        "meets_tax_credit_criteria_for_targeted_childcare_entitlement"
+    ]
+    
     def formula(benunit, period, parameters):
-        p = parameters(period).gov.dfe.targeted_childcare_entitlement
-
         # Check if household is in England
         country = benunit.household("country", period)
         in_england = country == country.possible_values.ENGLAND
-
-        # For Universal Credit recipients, we must use the definition of "earned income"
-        # The Local Authority (Duty to Secure Early Years Provision Free of Charge) (Amendment) Regulations 2018 - part 2.c
-        # https://www.legislation.gov.uk/uksi/2018/146/made
-
-        earned_income = benunit.sum(benunit.members("earned_income", period))
-
-        # Check Universal Credit eligibility
-        uc = benunit("universal_credit", period)
-        max_uc_income = p.max_income_uc_recipients
-        meets_uc = (uc > 0) & (earned_income <= max_uc_income)
-
-        # Check Tax Credits eligibility
-        # Legislation source for total (applicable) income limit:The Local Authority Regulations 2014, part 1.2.b
-        # https://www.legislation.gov.uk/uksi/2014/2147/regulation/1/made
-
-        # Reference for applicable income: The Tax Credits (Definition and Calculation of Income) Regulations 2002 s. 3
-
-        tax_credits_applicable_income = benunit(
-            "tax_credits_applicable_income", period
-        )
-        tax_credits = add(
-            benunit, period, ["child_tax_credit", "working_tax_credit"]
-        )
-        meets_tc = (tax_credits > 0) & (
-            tax_credits_applicable_income <= p.max_income_tc_recipients
-        )
-
+        
         # Check other qualifying benefits
-        has_qualifying_benefits = (
-            add(benunit, period, p.qualifying_benefits) > 0
-        )
-
-        # Determine overall benefits eligibility
-        return (
-            where(
-                uc > 0,
-                meets_uc,
-                where(tax_credits > 0, meets_tc, has_qualifying_benefits),
-            )
-            & in_england
-        )
+        p = parameters(period).gov.dfe.targeted_childcare_entitlement
+        has_qualifying_benefits = add(benunit, period, p.qualifying_benefits) > 0
+        
+        # Use the variable names directly instead of self.adds
+        meets_any_criteria = add(
+            benunit, 
+            period, 
+            [
+                "meets_universal_credit_criteria_for_targeted_childcare_entitlement",
+                "meets_tax_credit_criteria_for_targeted_childcare_entitlement"
+            ]
+        ) > 0
+        
+        return (meets_any_criteria | has_qualifying_benefits) & in_england
