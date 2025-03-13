7e2f688f8373fa2ef31080d72b6a31d029c313c6
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-03-13 12:23:56 +0000

    Make format

--- policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/meets_universal_credit_criteria_for_targeted_childcare_entitlement.py
+++ policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/meets_universal_credit_criteria_for_targeted_childcare_entitlement.py
@@ -6,7 +6,9 @@ class meets_universal_credit_criteria_for_targeted_childcare_entitlement(
 ):
     value_type = bool
     entity = BenUnit
-    label = "meets Universal Credit criteria for targeted childcare entitlement"
+    label = (
+        "meets Universal Credit criteria for targeted childcare entitlement"
+    )
     definition_period = YEAR
 
     def formula(benunit, period, parameters):
