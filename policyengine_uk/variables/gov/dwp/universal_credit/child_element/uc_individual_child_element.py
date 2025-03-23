fb651de78d17ecb31530256b8d38c0580c428d83
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-03-23 23:04:04 +0000

    Fix tests

--- policyengine_uk/variables/gov/dwp/universal_credit/child_element/uc_individual_child_element.py
+++ policyengine_uk/variables/gov/dwp/universal_credit/child_element/uc_individual_child_element.py
@@ -36,11 +36,13 @@ class uc_individual_child_element(Variable):
             select(
                 [
                     (child_index == 1) & born_before_limit & is_eligible,
+                    (child_index == 1) & is_eligible,
                     (child_index == 2) & is_eligible,
                     is_eligible & born_before_limit,
                 ],
                 [
                     p.first.higher_amount,
+                    p.amount,
                     p.amount,
                     p.amount,
                 ],
