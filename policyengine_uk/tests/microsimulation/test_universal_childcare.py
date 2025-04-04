50b3a9b4f3734f3627065f530bfd30fc2acbf148
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-04-04 11:35:43 +0100

    Hard code takeup rate

--- policyengine_uk/tests/microsimulation/test_universal_childcare.py
+++ policyengine_uk/tests/microsimulation/test_universal_childcare.py
@@ -6,13 +6,19 @@ def test_universal_childcare_entitlement_aggregate():
     """
     Test that the total universal childcare entitlement has a reasonable value.
     """
-    sim = Simulation(scope="macro", country="uk", time_period= "2024")
+    sim = Simulation(scope="macro", country="uk", time_period="2024")
     sim = sim.baseline_simulation
     year = 2024
     
+    # Hard coded take-up rate
+    # Move this take-up rate to the uk-data package
+    universal_childcare_entitlement_take_up_rate = 0.6
+    
     # Calculate individual entitlements and sum them up
     individual_entitlements = sim.calculate("universal_childcare_entitlement", period=year)
-    total_entitlement = individual_entitlements.sum()
+    
+    # Apply the take-up rate to the total
+    total_entitlement = individual_entitlements.sum() * universal_childcare_entitlement_take_up_rate
     
     lower_bound = 2_200_000_000
     upper_bound = 3_000_000_000
