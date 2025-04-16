5bc8b0f85b69b8d7800df5e8940451bd3122e155
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-04-16 12:41:42 +0100

    Add universal variable

--- policyengine_uk/variables/gov/dfe/extended_childcare_entitlement/is_eligible_child_for_extended_childcare.py
+++ policyengine_uk/variables/gov/dfe/extended_childcare_entitlement/is_eligible_child_for_extended_childcare.py
@@ -10,17 +10,17 @@ class is_eligible_child_for_extended_childcare(Variable):
     def formula(person, period, parameters):
         # Get child's age
         age = person("age", period)
-        
+
         # Get the parameters for extended childcare entitlement hours by age
         p = parameters(period).gov.dfe.extended_childcare_entitlement
-        
+
         # Check if hours > 0 for this age (using the hours parameter)
         hours_by_age = p.hours.calc(age)
-        
+
         # Get the benefit unit's extended childcare entitlement amount
         benunit = person.benunit
         entitlement_amount = benunit("extended_childcare_entitlement", period)
-        
+
         # Child is eligible if:
         # 1. Hours > 0 for this age AND
         # 2. Benefit unit's entitlement amount > 0
