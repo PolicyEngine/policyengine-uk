bedcb52b6c46e6252c1c76f85c0127168e2d6074
Vahid Ahmadi <57112303+vahid-ahmadi@users.noreply.github.com>
2025-05-22 14:19:12 +0100

    Edit Pension Credit income sources (#1131)
    * add seperate earnings file
    
    * add changelog
    
    * lowercase

---
+++ policyengine_uk/variables/gov/dwp/pension_credit/pension_credit_earnings.py
@@ -0,0 +1,11 @@
+from policyengine_uk.model_api import *
+
+
+class pension_credit_earnings(Variable):
+    label = "earnings for Pension Credit"
+    entity = BenUnit
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+    reference = "https://www.legislation.gov.uk/uksi/2002/1792/regulation/17A"
+    adds = "gov.dwp.pension_credit.income.earnings_sources"
