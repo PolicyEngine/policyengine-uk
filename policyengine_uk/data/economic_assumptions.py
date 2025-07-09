4b031666c9b230dda99ec9050e7691022fd683de
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-07-09 12:36:48 +0100

    Add function to store growth factors

---
+++ policyengine_uk/data/economic_assumptions.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from pathlib import Path
+
+START_YEAR = 2020
+END_YEAR = 2034
+
+
+def create_policyengine_uprating_factors_table():
+    from policyengine_uk.system import system
+
+    df = pd.DataFrame()
+
+    variable_names = []
+    years = []
+    index_values = []
+
+    for variable in system.variables.values():
+        if variable.uprating is not None:
+            parameter = system.parameters.get_child(variable.uprating)
+            start_value = parameter(START_YEAR)
+            for year in range(START_YEAR, END_YEAR + 1):
+                variable_names.append(variable.name)
+                years.append(year)
+                growth = parameter(year) / start_value
+                index_values.append(round(growth, 3))
+
+    df["Variable"] = variable_names
+    df["Year"] = years
+    df["Value"] = index_values
+
+    # Convert to there is a column for each year
+    df = df.pivot(index="Variable", columns="Year", values="Value")
+    df = df.sort_values("Variable")
+
+    # Create a table with growth factors by year
+
+    df_growth = df.copy()
+    for year in range(END_YEAR, START_YEAR, -1):
+        df_growth[year] = round(df_growth[year] / df_growth[year - 1] - 1, 3)
+    df_growth[START_YEAR] = 0
+
+    df_growth.to_csv(Path(__file__).parent / "uprating_growth_factors.csv")
+    return df
+
+
+if __name__ == "__main__":
+    create_policyengine_uprating_factors_table()
