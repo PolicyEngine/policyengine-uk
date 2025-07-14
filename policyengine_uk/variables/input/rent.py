1c23026bbfdd7b7cf5a1254bf3d7a814f0212bfe
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2025-07-14 15:10:16 +0100

    Improve rent uprating (#1227)
    * Improve rent uprating
    
    * Add safety check
    
    * Add links to docs

--- policyengine_uk/variables/input/rent.py
+++ policyengine_uk/variables/input/rent.py
@@ -11,4 +11,43 @@ class rent(Variable):
     value_type = float
     unit = GBP
     quantity_type = FLOW
-    uprating = "gov.economic_assumptions.indices.obr.rent"
+
+    def formula(household, period, parameters):
+        if period.start.year < 2023:
+            # We don't have growth rates for rent before this.
+            return 0
+
+        if household.simulation.dataset is None:
+            return 0
+
+        data_year = household.simulation.dataset.time_period
+        original_rent = household("rent", data_year)
+        tenure_type = household("tenure_type", period).decode_to_str()
+
+        is_social_rent = (tenure_type == "RENT_FROM_COUNCIL") | (
+            tenure_type == "RENT_FROM_HA"
+        )
+
+        is_private_rent = tenure_type == "RENT_PRIVATELY"
+
+        obr = parameters.gov.economic_assumptions.indices.obr
+
+        private_rent_uprating = obr.lagged_average_earnings(
+            period
+        ) / obr.lagged_average_earnings(data_year)
+        social_rent_uprating = obr.social_rent(period) / obr.social_rent(
+            data_year
+        )
+
+        return select(
+            [
+                is_social_rent,
+                is_private_rent,
+                True,
+            ],
+            [
+                original_rent * social_rent_uprating,
+                original_rent * private_rent_uprating,
+                original_rent,
+            ],
+        )
