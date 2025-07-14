2f9d3ff13d6fa87753a5c12d66d17a5092d2561c
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-07-14 15:21:23 +0100

    Fix typo

--- policyengine_uk/parameters/gov/economic_assumptions/lag_cpi.py
+++ policyengine_uk/parameters/gov/economic_assumptions/lag_cpi.py
@@ -11,7 +11,7 @@ def add_lagged_cpi(
     cpi = obr.consumer_price_index
 
     lagged_cpi = Parameter(
-        "gov.economic_assumptions.yoy_growth.lagged_cpi",
+        "gov.economic_assumptions.yoy_growth.obr.lagged_cpi",
         data={
             "values": {
                 f"{year}-01-01": cpi(year - 1) for year in range(2022, 2030)
