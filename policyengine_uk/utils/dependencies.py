2854d91812f7f61c8df807b9934999bf84ca69dd
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-07-17 17:05:49 +0100

    Update chart functions

--- policyengine_uk/utils/dependencies.py
+++ policyengine_uk/utils/dependencies.py
@@ -1,24 +1,25 @@
 import inspect
 import re
 from policyengine_uk.system import system
+import plotly.express as px
+import pandas as pd
+from plotly import graph_objects as go
 
-def get_variable_dependencies(variable_name):
-    # First, check for adds/subtracts
-    variable = system.get_variable(variable_name)
-    deps = []
-    if variable.adds:
-        deps.extend(variable.adds)
-    if variable.subtracts:
-        deps.extend(variable.subtracts)
-    # Then, check for formula
-
-
-    formula = variable.get_formula(2025)
-    if formula is not None:
-        formula_source = inspect.getsource(formula)
-        deps.extend(extract_variables_regex(formula_source))
+def find_variable_in_tree(variable_name, node):
+    if node.name == variable_name:
+        return node
+    for child in node.children:
+        result = find_variable_in_tree(variable_name, child)
+        if result:
+            return result
     
-    return deps
+    raise ValueError(f"Variable '{variable_name}' not found in the simulation tree. The *first thing* you asked the simulation to calculate needs to include your target variable in its computation tree.")
+
+def get_variable_dependencies(variable_name, sim):
+    node = find_variable_in_tree(variable_name, sim.tracer.trees[0])
+    if not node:
+        return []
+    return [child.name for child in node.children]
 
 def extract_variables_regex(formula_source):
     # Find all strings in double quotes that look like variable names
@@ -29,13 +30,14 @@ def extract_variables_regex(formula_source):
 def calculate_dependency_contributions(sim, variable_name, year):
     original_values = sim.calculate(variable_name, year)
     dependency_contributions = {}
-    for variable in get_variable_dependencies(variable_name):
+    first_level_dependencies = get_variable_dependencies(variable_name, sim)
+    for variable in first_level_dependencies:
+        if "weight" in variable:
+            continue
         sim.get_holder(variable_name).delete_arrays(year)
         value_type = system.get_variable(variable).value_type
         current_values = sim.calculate(variable, year)
-        if value_type == bool:
-            sim.set_input(variable, year, (current_values * 0).astype(bool))
-        elif value_type == float:
+        if value_type == float:
             sim.set_input(variable, year, (current_values * 0).astype(float))
         
         new_values = sim.calculate(variable_name, year)
@@ -44,4 +46,65 @@ def calculate_dependency_contributions(sim, variable_name, year):
         sim.set_input(variable_name, year, original_values)
         sim.set_input(variable, year, current_values)
     
-    return dependency_contributions
+    return pd.Series(dependency_contributions)
+
+
+
+def calculate_dependency_contribution_change(baseline_sim, reform_sim, variable_name, year, top_n = 5):
+    baseline_dependency = calculate_dependency_contributions(baseline_sim, variable_name, year)
+    reform_dependency = calculate_dependency_contributions(reform_sim, variable_name, year)
+
+    df = pd.DataFrame({
+        'baseline': pd.Series(baseline_dependency),
+        'reform': pd.Series(reform_dependency)
+    }).fillna(0)
+
+    df['change'] = df['reform'] - df['baseline']
+    df['relative_change'] = df['change'] / df['baseline'].abs().replace(0, 1)
+
+    # Keep the top N variables by absolute change
+    if top_n is not None:
+        df = df.reindex(df['change'].abs().nlargest(top_n).index)
+
+    return df.sort_values(by='change', ascending=False)
+
+def create_waterfall_chart(sim, variable_name, year, top_n=5):
+    if not sim.trace:
+        raise ValueError("Simulation must have trace enabled to create a waterfall chart.")
+
+    df = calculate_dependency_contributions(sim, variable_name, year)
+
+    # make a waterfall chart
+
+    fig = go.Figure(go.Waterfall(
+        name="Waterfall",
+        orientation="v",
+        measure=["relative"] * len(df),
+        x=df.index,
+        y=df.values,
+        connector={"line": {"color": "rgb(63, 63, 63)", "width": 2}},
+        increasing={"marker": {"color": "green"}},
+        decreasing={"marker": {"color": "red"}},
+        totals={"marker": {"color": "blue"}}
+    ))
+    return fig
+
+def create_waterfall_change_chart(sim_1, sim_2, variable_name, year, top_n=5):
+    df = calculate_dependency_contribution_change(
+        sim_1, sim_2, variable_name, year, top_n=top_n
+    )
+
+    # make a waterfall chart
+
+    fig = go.Figure(go.Waterfall(
+        name="Waterfall",
+        orientation="v",
+        measure=["relative"] * len(df),
+        x=df.index,
+        y=df.change,
+        connector={"line": {"color": "rgb(63, 63, 63)", "width": 2}},
+        increasing={"marker": {"color": "green"}},
+        decreasing={"marker": {"color": "red"}},
+        totals={"marker": {"color": "blue"}}
+    ))
+    return fig
