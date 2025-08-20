93b0c43f4e77a9d3df6903c74cc207c27ae2018b
Max Ghenis <mghenis@gmail.com>
2025-08-20 08:40:01 -0400

    Add firm_weight and firm_count variables
    The test_validity.py test expects all entities to have a weight variable.
    Added firm_weight and firm_count variables to support microsimulation weighting.

---
+++ policyengine_uk/variables/gov/hmrc/vat/firm/firm_count.py
@@ -0,0 +1,15 @@
+from policyengine_core.variables import Variable
+from policyengine_uk.entities import Firm
+import numpy as np
+
+
+class firm_count(Variable):
+    value_type = float
+    entity = Firm
+    label = "Number of firms"
+    definition_period = "year"
+    documentation = "Count of firms represented (typically 1 for individual firm records)"
+
+    def formula(firm, period, parameters):
+        # Each firm record represents one firm by default
+        return np.ones(firm.count)
