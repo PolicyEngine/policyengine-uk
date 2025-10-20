362992ff54a3e6b668914f5df9ac82d48a9278f0
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-10-20 13:35:49 +0100

    Fix minor issue, fallback

--- policyengine_uk/variables/gov/dwp/additional_state_pension.py
+++ policyengine_uk/variables/gov/dwp/additional_state_pension.py
@@ -12,8 +12,10 @@ class additional_state_pension(Variable):
         simulation = person.simulation
         if simulation.dataset is None:
             return 0
-
-        data_year = min(simulation.dataset.years)
+        try:
+            data_year = min(simulation.dataset.years)
+        except:
+            data_year = period.year
         reported = person("state_pension_reported", data_year) / WEEKS_IN_YEAR
         type = person("state_pension_type", data_year)
         maximum_basic_sp = parameters(
