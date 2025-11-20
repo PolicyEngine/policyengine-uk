1b426d25a5dd634a57755a13a0b7c31d7ef5b4c5
Vahid Ahmadi <57112303+vahid-ahmadi@users.noreply.github.com>
2025-11-20 14:27:19 +0000

    Revert "Salary sacrifice pension modeling"

--- policyengine_uk/variables/input/employment_income.py
+++ policyengine_uk/variables/input/employment_income.py
@@ -14,6 +14,5 @@ class employment_income(Variable):
         "employment_income_before_lsr",
         "employment_income_behavioral_response",
         "employer_ni_fixed_employer_cost_change",
-        "salary_sacrifice_returned_to_income",
     ]
     uprating = "gov.economic_assumptions.indices.obr.average_earnings"
