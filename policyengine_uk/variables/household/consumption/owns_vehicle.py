f07da59de63e6d919eae9f6f036e1a03d9261c38
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2025-12-01 12:50:41 +0000

    Add owns vehicle variable

---
+++ policyengine_uk/variables/household/consumption/owns_vehicle.py
@@ -0,0 +1,19 @@
+from policyengine_uk.model_api import *
+
+
+class owns_vehicle(Variable):
+    label = "whether the household owns a vehicle"
+    documentation = (
+        "Derived from fuel spending. Used for calibration to NTS 2024 data "
+        "showing 78% of UK households own at least one car. "
+        "Source: https://www.gov.uk/government/statistics/national-travel-survey-2024"
+    )
+    reference = "https://www.gov.uk/government/statistics/national-travel-survey-2024/nts-2024-household-car-availability-and-trends-in-car-trips"
+    entity = Household
+    definition_period = YEAR
+    value_type = bool
+
+    def formula(household, period):
+        petrol = household("petrol_spending", period)
+        diesel = household("diesel_spending", period)
+        return (petrol + diesel) > 0
