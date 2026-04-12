c21a8516d126550f9611d3b496b59f352caa752d
Max Ghenis <mghenis@gmail.com>
2026-04-12 08:31:20 -0400

    Move ESA and JSA lower threshold under tariff income

--- policyengine_uk/variables/gov/dwp/esa_income_tariff_income.py
+++ policyengine_uk/variables/gov/dwp/esa_income_tariff_income.py
@@ -13,6 +13,6 @@ class esa_income_tariff_income(Variable):
         ESA = parameters(period).gov.dwp.ESA.income
         capital = benunit("esa_income_assessable_capital", period)
         mt = ESA.capital
-        excess_capital = max_(0, capital - mt.lower_threshold)
+        excess_capital = max_(0, capital - mt.tariff_income.lower_threshold)
         tariff_units = np.ceil(excess_capital / mt.tariff_income.step)
         return tariff_units * mt.tariff_income.amount * WEEKS_IN_YEAR
