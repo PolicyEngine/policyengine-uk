00ea982dd77368d4f573329e441e1ce283723083
Max Ghenis <mghenis@gmail.com>
2026-04-12 08:41:09 -0400

    Rename DWP capital params to limit and threshold

--- policyengine_uk/variables/gov/dwp/universal_credit/is_uc_eligible.py
+++ policyengine_uk/variables/gov/dwp/universal_credit/is_uc_eligible.py
@@ -10,8 +10,8 @@ class is_uc_eligible(Variable):
 
     def formula(benunit, period, parameters):
         capital = benunit("uc_assessable_capital", period)
-        capital_limit = parameters(
+        limit = parameters(
             period
-        ).gov.dwp.universal_credit.means_test.capital.capital_limit
+        ).gov.dwp.universal_credit.means_test.capital.limit
         has_working_age_adult = benunit.any(benunit.members("is_WA_adult", period))
-        return has_working_age_adult & (capital <= capital_limit)
+        return has_working_age_adult & (capital <= limit)
