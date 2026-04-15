99986839cc1a487861dec7bf41a3b0fd866fbb5d
Max Ghenis <mghenis@gmail.com>
2026-04-15 17:43:11 -0400

    Update targeted childcare tax credit criteria

--- policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/meets_tax_credit_criteria_for_targeted_childcare_entitlement.py
+++ policyengine_uk/variables/gov/dfe/targeted_childcare_entitlement/meets_tax_credit_criteria_for_targeted_childcare_entitlement.py
@@ -9,17 +9,16 @@ class meets_tax_credit_criteria_for_targeted_childcare_entitlement(Variable):
 
     def formula(benunit, period, parameters):
         p = parameters(period).gov.dfe.targeted_childcare_entitlement
-
-        tax_credits = add(benunit, period, ["child_tax_credit", "working_tax_credit"])
-
-        tax_credits_applicable_income = benunit("tax_credits_applicable_income", period)
-
-        # Check Tax Credits eligibility
-        # Legislation source for total (applicable) income limit:The Local Authority Regulations 2014, part 1.2.b
-        # https://www.legislation.gov.uk/uksi/2014/2147/regulation/1/made
-
-        # Reference for applicable income: The Tax Credits (Definition and Calculation of Income) Regulations 2002 s. 3
-
-        return (tax_credits > 0) & (
-            tax_credits_applicable_income <= p.income_limit.tax_credits
-        )
+        tax_credit_criteria = [
+            "meets_working_tax_credit_criteria_for_targeted_childcare_entitlement",
+            "meets_child_tax_credit_criteria_for_targeted_childcare_entitlement",
+            "working_tax_credit_run_on",
+        ]
+        active_tax_credit_criteria = [
+            variable
+            for variable in tax_credit_criteria
+            if variable in p.qualifying_criteria
+        ]
+        if not active_tax_credit_criteria:
+            return False
+        return add(benunit, period, active_tax_credit_criteria) > 0
