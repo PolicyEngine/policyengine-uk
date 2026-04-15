275c500ccc1b5d25d669f64086a0a879323ee08b
Max Ghenis <mghenis@gmail.com>
2026-04-15 10:04:46 -0400

    Remove inferred is_parent formula (#1595)

--- policyengine_uk/variables/household/demographic/is_parent.py
+++ policyengine_uk/variables/household/demographic/is_parent.py
@@ -6,25 +6,4 @@ class is_parent(Variable):
     entity = Person
     label = "Whether this person is a parent in their benefit unit"
     definition_period = YEAR
-
-    def formula(person, period, parameters):
-        benunit = person.benunit
-        age = person("age", period)
-        family_type = benunit("family_type", period)
-
-        # Find two oldest members
-        benunit_ages = benunit.members("age", period)
-        adult_index = person("adult_index", period)
-
-        # Get family types enum
-        family_types = family_type.possible_values
-
-        # For lone parents (FamilyType.LONE_PARENT), only the eldest is parent
-        is_lone_parent = (family_type == family_types.LONE_PARENT) & (adult_index == 1)
-
-        # For couples with children (FamilyType.COUPLE_WITH_CHILDREN), two eldest are parents
-        is_couple_parent = (family_type == family_types.COUPLE_WITH_CHILDREN) & (
-            (adult_index == 1) | (adult_index == 2)
-        )
-
-        return is_lone_parent | is_couple_parent
+    default_value = False
