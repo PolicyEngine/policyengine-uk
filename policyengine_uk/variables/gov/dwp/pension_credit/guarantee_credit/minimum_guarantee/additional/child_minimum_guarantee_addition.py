eb2585fe19a89d2a780435b732f843638da9cc8e
Max Ghenis <mghenis@gmail.com>
2026-05-23 09:27:58 -0400

    Update 2026-27 UK benefit and tax parameters (#1691)

--- policyengine_uk/variables/gov/dwp/pension_credit/guarantee_credit/minimum_guarantee/additional/child_minimum_guarantee_addition.py
+++ policyengine_uk/variables/gov/dwp/pension_credit/guarantee_credit/minimum_guarantee/additional/child_minimum_guarantee_addition.py
@@ -14,6 +14,17 @@ class child_minimum_guarantee_addition(Variable):
         is_child = person(
             "is_child_or_qualifying_young_person_for_pension_credit", period
         )
+        child_index = (
+            person.get_rank(
+                person.benunit,
+                -person("age", period),
+                condition=is_child,
+            )
+            + 1
+        )
+        first_child_born_before_2017 = (child_index == 1) & (
+            person("birth_year", period) < 2017
+        )
         gc = parameters(period).gov.dwp.pension_credit.guarantee_credit
         standard_disability_benefits = gc.child.disability.eligibility
         severe_disability_benefits = gc.child.disability.severe.eligibility
@@ -21,6 +32,11 @@ class child_minimum_guarantee_addition(Variable):
         is_severely_disabled = add(person, period, severe_disability_benefits) > 0
         is_standard_disabled = is_disabled & ~is_severely_disabled
         is_not_disabled = ~is_disabled
+        child_addition = where(
+            first_child_born_before_2017,
+            gc.child.first.addition,
+            gc.child.addition,
+        )
         per_child_amount = (
             select(
                 [
@@ -29,9 +45,9 @@ class child_minimum_guarantee_addition(Variable):
                     is_child & is_severely_disabled,
                 ],
                 [
-                    gc.child.addition,
-                    gc.child.addition + gc.child.disability.addition,
-                    gc.child.addition + gc.child.disability.severe.addition,
+                    child_addition,
+                    child_addition + gc.child.disability.addition,
+                    child_addition + gc.child.disability.severe.addition,
                 ],
             )
             * WEEKS_IN_YEAR
