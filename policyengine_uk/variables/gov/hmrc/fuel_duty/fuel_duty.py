ac9cb69231e64845738d70d955a8b16fafc9f96e
Vahid Ahmadi <57112303+vahid-ahmadi@users.noreply.github.com>
2026-05-23 15:13:14 +0100

    Add rural fuel duty relief (#1672)
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- policyengine_uk/variables/gov/hmrc/fuel_duty/fuel_duty.py
+++ policyengine_uk/variables/gov/hmrc/fuel_duty/fuel_duty.py
@@ -12,4 +12,8 @@ class fuel_duty(Variable):
         fd = parameters(period).gov.hmrc.fuel_duty
         petrol_litres = household("petrol_litres", period.this_year) / MONTHS_IN_YEAR
         diesel_litres = household("diesel_litres", period.this_year) / MONTHS_IN_YEAR
-        return fd.petrol_and_diesel * (petrol_litres + diesel_litres)
+        in_relief_area = household("in_rural_fuel_duty_relief_area", period.this_year)
+        effective_rate = (
+            fd.petrol_and_diesel - in_relief_area * fd.rural_fuel_duty_relief
+        )
+        return effective_rate * (petrol_litres + diesel_litres)
