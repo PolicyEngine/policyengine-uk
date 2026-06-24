8eaab780f2a085577038c55e35dd12248eabd13f
Vahid Ahmadi <va.vahidahmadi@gmail.com>
2026-06-24 14:41:20 +0100

    Format ISA variables with ruff

--- policyengine_uk/variables/household/wealth/stocks_and_shares_isa.py
+++ policyengine_uk/variables/household/wealth/stocks_and_shares_isa.py
@@ -4,8 +4,7 @@ from policyengine_uk.model_api import *
 class stocks_and_shares_isa(Variable):
     label = "stocks and shares ISA holdings"
     documentation = (
-        "Amount held in stocks and shares (investment) Individual Savings "
-        "Accounts"
+        "Amount held in stocks and shares (investment) Individual Savings Accounts"
     )
     entity = Household
     definition_period = YEAR
